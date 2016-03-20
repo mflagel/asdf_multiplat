c73fe794e2eeeebacaeacd2e0ef83adb9f082356
Mathias Flagel <mathias.flagel@gmail.com>
2016-03-19 23:29:14 -0700

    threw a basic character_skills_layout_t struct in the cpp to hold stuff for that tab. Not sure if it deserves its own file yet

--- gurps_track/src/ui/main_layout.cpp
+++ gurps_track/src/ui/main_layout.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "main_layout.h"
 
+#include "ui/skill_list_layout.h"
 #include "data/character.h"
 
 using namespace std;
@@ -13,6 +14,25 @@ using namespace data;
 
 namespace ui
 {
+	struct character_skills_layout_t : mk::Page
+	{
+		skill_list_t all_skills;
+
+		character_skills_layout_t(data::character_t &);
+	};
+
+	character_skills_layout_t::character_skills_layout_t(data::character_t& _character)
+	: Page("Skills")
+	, all_skills(skill_list_t("../assets/data/gurps_skills.json"))
+	{
+		Tabber& tabs = emplace<Tabber>();
+
+		Page& learned = tabs.emplace<Page>("Learned");
+		learned.emplace<character_skill_list_t>(_character);
+
+		Page& all = tabs.emplace<Page>("All");
+		all.emplace<skill_library_t>(all_skills, &_character);
+	}
 
 	main_layout_t::main_layout_t(data::character_t& _character)
 	: Board()
@@ -23,13 +43,8 @@ namespace ui
 		character.emplace<character_main_layout_t>(_character);
 
 		Page& combat    = tabber.emplace<Page>("Stats");
-
 		Page& traits    = tabber.emplace<Page>("Traits");
-
-		Page& skills    = tabber.emplace<Page>("Skills");
-		skills.emplace<Button>("Add Skills");
-		character.emplace<character_skill_list_t>(_character);
-
+		Page& skills    = tabber.emplace<character_skills_layout_t>(_character);
 		Page& spells    = tabber.emplace<Page>("Spells");
 	}
 
