e2e4787a5c24ca0a1d9efcc9a660c97b9c92071d
Mathias Flagel <mathias.flagel@gmail.com>
2016-03-19 18:55:16 -0700

    added character_skills_layout (which is mostly a copy of skill_library_t) and main_layout, which will act as the root layout (eventually)

---
+++ gurps_track/src/ui/main_layout.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "main_layout.h"
+
+#include "data/character.h"
+
+using namespace std;
+using namespace mk;
+
+namespace gurps_track
+{
+
+using namespace data;
+
+namespace ui
+{
+
+	main_layout_t::main_layout_t(data::character_t& _character)
+	: Board()
+	{
+		Tabber& tabber = emplace<Tabber>();
+
+		Page& character = tabber.emplace<Page>("Character");
+		character.emplace<character_main_layout_t>(_character);
+
+		Page& combat    = tabber.emplace<Page>("Stats");
+		Page& traits    = tabber.emplace<Page>("Traits");
+		Page& skills    = tabber.emplace<Page>("Skills");
+		Page& spells    = tabber.emplace<Page>("Spells");
+	}
+
+}
+}
---
+++ gurps_track/src/ui/main_layout.h
@@ -0,0 +1,24 @@
+#include <kiui/Ui/mkUiConfig.h>
+#include <kiui/Ui/mkUiTypes.h>
+
+#include "character_main_layout.h"
+#include "character_skills_layout.h"
+
+namespace gurps_track
+{
+
+namespace data
+{
+	struct character_t;
+}
+
+namespace ui
+{
+
+	struct main_layout_t : mk::Board
+	{
+		main_layout_t(data::character_t&);
+	};
+
+}
+}
