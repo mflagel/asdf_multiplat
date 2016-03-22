6839d35259295508e4d08e769a0233b0557b57f9
Mathias Flagel <mathias.flagel@gmail.com>
2016-03-22 16:17:20 -0700

    added character combat UI. Everything is in the character namespace, rather than having 'character_' in front of all struct names. Most of the structs are stubs, since some of the backend logic doesnt exist yet (ex: anything based on player gear

---
+++ gurps_track/src/character_combat.cpp
@@ -0,0 +1,219 @@
+#include "stdafx.h"
+#include "character_combat.h"
+
+#include "data/character.h"
+
+using namespace std;
+using namespace mk;
+
+namespace gurps_track
+{
+
+using namespace data;
+
+namespace ui
+{
+namespace character
+{
+	combat_t::combat_t(data::character_t& _character)
+	: Board()
+	{
+		emplace<stats_t>         (_character);
+		emplace<encumberance_t>  (_character);
+		emplace<armor_t>         (_character);
+		emplace<attacks_t>       (_character);
+		emplace<defenses_t>      (_character);
+		emplace<status_effects_t>(_character);
+		emplace<active_spells_t> (_character);
+	}
+
+
+	///
+	stats_t::stat_t::stat_t(std::string const& name)
+	{
+		Expandbox& expnd = emplace<Expandbox>(name, true);
+		value_label = &expnd.header()->emplace<Label>("-");
+		Sequence& sub_seq = expnd.emplace<Sequence>();
+		point_cost_label = &sub_seq.emplace<Label>("[-]");
+		sub_seq.emplace<Button>("+"); ///todo: button functionality
+		sub_seq.emplace<Button>("-"); ///todo: button functionality
+
+		expnd.collapse();
+	}
+
+	stats_t::base_stat_t::base_stat_t(data::character_t& _character, data::base_stat_e _stat)
+	: stats_t::stat_t(base_stat_names[_stat])
+	, character(_character)
+	, stat(_stat)
+	{
+		set_data(character);
+	}
+
+	void stats_t::base_stat_t::set_data(data::character_t const& character)
+	{
+		value_label->setLabel(to_string(character.base_stats[stat]));
+		point_cost_label->setLabel("["
+			+ to_string(get_stat_point_cost(character.base_stats[stat]))
+			+ "]");
+	}
+
+	stats_t::derived_stat_t::derived_stat_t(data::character_t& _character, data::derived_stat_e _stat)
+	: stat_t(derived_stat_names[_stat])
+	, character(_character)
+	, stat(_stat)
+	{
+		set_data(character);
+	}
+
+	void stats_t::derived_stat_t::set_data(data::character_t const& character)
+	{
+		value_label->setLabel(to_string(character.derived_stats[stat]));
+		point_cost_label->setLabel("[todo]");
+	}
+
+
+	///
+	stats_t::stats_t(data::character_t& _character)
+	: Expandbox{"Stats"}
+	{
+		GridSheet& stat_grid = emplace<GridSheet>(DIM_X);
+
+		Sequence& st_seq = stat_grid.emplace<Sequence>();
+		st 	    = &st_seq.emplace<base_stat_t>(_character, stat_ST);
+		fatigue = &st_seq.emplace<derived_stat_t>(_character, stat_fatigue);
+
+		dx = &stat_grid.emplace<base_stat_t>(_character, stat_DX);
+		iq = &stat_grid.emplace<base_stat_t>(_character, stat_IQ);
+
+		Sequence& ht_seq = stat_grid.emplace<Sequence>();
+		ht 	   = &ht_seq.emplace<base_stat_t>(_character, stat_HT);
+		health = &ht_seq.emplace<derived_stat_t>(_character, stat_HP);
+
+		set_data(_character);
+	}
+
+	void stats_t::set_data(data::character_t const& character)
+	{
+		// st->set_data(character);
+		// dx->set_data(character);
+		// iq->set_data(character);
+		// ht->set_data(character);
+		fatigue->value_label->setLabel( to_string(character.current_fatigue) + "/" + to_string(character.max_fatigue()) );
+		health-> value_label->setLabel( to_string(character.current_HP) + "/" + to_string(character.max_HP()) );
+	}
+
+
+	///
+	encumberance_t::encumberance_t(data::character_t& _character)
+	: Expandbox("Encumberance")
+	{
+		GridSheet& grid = emplace<GridSheet>(DIM_X);
+
+		for(size_t i = 0; i < encumberance_count; ++i)
+		{
+			Sequence& seq = grid.emplace<Sequence>();
+			seq.emplace<Label>(data::encumberance_names[i]);
+			encumberance_labels[i] = &seq.emplace<Label>("-");
+		}
+	}
+
+	void encumberance_t::set_data(data::character_t const&)
+	{
+		//TODO
+	}
+
+
+	///
+	armor_t::armor_t(data::character_t& _character)
+	: Expandbox("Armor")
+	{
+		StringVector col_names = {"-", "H", "B", "A", "L", "H", "F"/*, "A"*/};
+		Table& table = emplace<Table>( move(col_names), vector<float>(1.0f/data::armor_region_count) );
+
+		// ASSERT(col_names.size() == armor_region_count, "");
+
+		Sequence& pd_seq = table.emplace<Sequence>();
+		Sequence& dr_seq = table.emplace<Sequence>();
+		pd_seq.emplace<Label>("PD");
+		dr_seq.emplace<Label>("DR");
+
+		for(size_t i = 0; i < armor_region_count - 1; ++i)
+		{
+			pd_labels[i] = &pd_seq.emplace<Label>("-");
+			dr_labels[i] = &dr_seq.emplace<Label>("-");
+		}
+
+		// other_armor = &emplace<Label>("-");
+
+		set_data(_character);
+	}
+
+	void armor_t::set_data(data::character_t const& character)
+	{
+		for(size_t i = 0; i < character.armor_info.size(); ++i)
+		{
+			auto const& armor = character.armor_info[i + 1];
+			// pd_labels[i]->setLabel(to_string(armor.PD_mod));
+			// dr_labels[i]->setLabel(to_string(armor.DR_mod));
+		}
+	}
+
+
+	///
+	attacks_t::attacks_t(data::character_t&)
+	: Expandbox("Attacks")
+	{
+	}
+
+	void attacks_t::set_data(data::character_t const&)
+	{
+	}
+
+
+	///
+	defenses_t::defenses_t(data::character_t& _character)
+	: Expandbox("Defenses")
+	{
+		Sequence& dodge_seq = emplace<Sequence>();
+		dodge_seq.emplace<Label>("Dodge: ");
+		dodge = &dodge_seq.emplace<Label>("-");
+
+		Sequence& block_seq = emplace<Sequence>();
+		block_seq.emplace<Label>("Dodge: ");
+		block = &block_seq.emplace<Label>("-");
+
+		std::vector<mk::Expandbox*> weapon_parries;
+
+
+		set_data(_character);
+	}
+
+	void defenses_t::set_data(data::character_t const&)
+	{
+	}
+
+
+	///
+	status_effects_t::status_effects_t(data::character_t&)
+	: Expandbox("Status Effects")
+	{
+	}
+
+	void status_effects_t::set_data(data::character_t const&)
+	{
+	}
+
+
+	///
+	active_spells_t::active_spells_t(data::character_t&)
+	: Expandbox("Active Spells")
+	{
+	}
+
+	void active_spells_t::set_data(data::character_t const&)
+	{
+	}
+
+}
+}
+}
---
+++ gurps_track/src/character_combat.h
@@ -0,0 +1,218 @@
+#include <kiui/Ui/mkUiConfig.h>
+#include <kiui/Ui/mkUiTypes.h>
+
+#include "data/basic_data.h"
+#include "data/character.h"
+
+namespace gurps_track
+{
+
+namespace ui
+{
+namespace character
+{
+	/*
+	+-----------------+
+	| ST  8  FT  8/8  |
+	| DX 11           |
+	| IQ 14           |
+	| HT 10  HP 10/10 |
+	+-----------------+
+	| Spd: 6.25  Mv 6 |
+	| Init: X  Enc: H |
+	+-----------------+
+	|- Encumberance  -|
+	| None:           |
+	| Light:          |
+	| Medium:         |
+	| ....            |
+	+-----------------+
+	|-    Armour     -|
+	|   H B A L H F A |
+	|PD 0 2 2 1 2 2   |
+	|DR 0 3 2 1 2 2   |
+	|other pd other dr|
+	+-----------------+
+	|-    Damage     -|
+	|Thr: 1d-1  Sw:1d |
+	|Kick: 1d-1       |
+	+-----------------+
+	|-    Attacks    -|
+	|- Large Knife   -|
+	|   Cut           |
+	|    {cut stats}  |
+	|   Imp           |
+	|    {imp stats}  |
+	|- Small Knife   -|
+	|- Dagger        -|
+	|                 |
+	|- Poltergeist   -|
+	|   {ranged stats}|
+	|                 |
+	|- Ammunition    -|
+	+-----------------+
+	|-   Defenses    -|
+	| Dodge: 6 + PD   |
+	|-Parry: 7 + PD  -|  // default collapsed
+	|  Weapon 1 Parry |
+	|  Weapon 2 Parry |
+	|  Weapon 3 Parry |
+	| Block: 5 + PD   |
+	+-----------------+
+	|- Status Effects-|
+	| Poisoned        |
+	|   HP -= 2/s     |
+	| Deafened        |
+	+-----------------+
+	|- Active Spells -|
+	|  Penalty: -##   |
+	|-  Illusions ## -|
+	|- Simple  2      |
+	|    Description  |
+	|      +5  +3A    |  // spell pass by 5, art pass by 3
+	|    Description  |
+	|      +1  -4A    |
+	|- Complex  1     |
+	|    Description  |
+	|      +2  +2A    |  // spell pass by 5, art pass by 3
+	|- Keen Eyes 3    |
+	|    Description  |  // ex: when spell was casted
+	|      +2         |
+	|- Levitation     |
+	|    Weight: 90lb |
+	|    +10          |
+	+-----------------+
+	*/
+
+	struct stats_t : mk::Expandbox
+	{
+		struct stat_t : mk::Sequence
+		{
+			mk::Label* value_label;
+			mk::Label* point_cost_label;
+
+			stat_t(std::string const& name);
+		};
+
+		struct base_stat_t : stat_t
+		{
+			data::character_t& character;
+			data::base_stat_e stat;
+
+			base_stat_t(data::character_t&, data::base_stat_e);
+
+			void set_data(data::character_t const&);
+		};
+
+		struct derived_stat_t : stat_t
+		{
+			data::character_t& character;
+			data::derived_stat_e stat;
+
+			derived_stat_t(data::character_t&, data::derived_stat_e);
+
+			void set_data(data::character_t const&);
+		};
+
+		stat_t* st = nullptr;
+		stat_t* dx = nullptr;
+		stat_t* iq = nullptr;
+		stat_t* ht = nullptr;
+
+		stat_t* fatigue = nullptr;
+		stat_t* health  = nullptr;
+
+		stats_t(data::character_t&);
+
+		void set_data(data::character_t const&);
+	};
+
+	struct movement_t : mk::Expandbox
+	{
+		mk::Label* speed        = nullptr;
+		mk::Label* move         = nullptr;
+		mk::Label* initiative   = nullptr;
+		mk::Label* encumberance = nullptr;
+
+		movement_t(data::character_t&);
+	};
+
+	struct encumberance_t : mk::Expandbox
+	{
+		std::array<mk::Label*, data::encumberance_count> encumberance_labels;
+
+		encumberance_t(data::character_t&);
+
+		void set_data(data::character_t const&);
+	};
+
+	struct armor_t : mk::Expandbox
+	{
+		std::array<mk::Label*, data::armor_region_count-1> pd_labels;  // -1 to ignore armor_region_none
+		std::array<mk::Label*, data::armor_region_count-1> dr_labels;
+		mk::Label* other_armor;
+
+		armor_t(data::character_t&);
+
+		void set_data(data::character_t const&);
+	};
+
+	struct attacks_t : mk::Expandbox
+	{
+		std::vector<mk::Expandbox*> attack_boxes;
+
+		attacks_t(data::character_t&);
+
+		void set_data(data::character_t const&);
+	};
+
+	struct defenses_t : mk::Expandbox
+	{
+		mk::Label* dodge = nullptr;
+		mk::Label* block = nullptr;
+		std::vector<mk::Expandbox*> weapon_parries;
+
+		defenses_t(data::character_t&);
+
+		void set_data(data::character_t const&);
+	};
+
+	struct status_effects_t : mk::Expandbox
+	{
+		struct effect_t : mk::Expandbox
+		{
+			std::vector<mk::Label*> modifiers;
+
+			effect_t(data::status_effect_t);
+		};
+
+		std::vector<effect_t> effects;
+
+		status_effects_t(data::character_t&);
+
+		void set_data(data::character_t const&);
+	};
+
+	struct active_spells_t : mk::Expandbox
+	{
+		struct active_spell_t : mk::Expandbox
+		{
+			mk::Label* description = nullptr;
+			std::vector<mk::Label> modifiers;
+		};
+
+		mk::Label* num_active_spells = nullptr;
+
+		active_spells_t(data::character_t&);
+
+		void set_data(data::character_t const&);
+	};
+
+
+	struct combat_t : mk::Board
+	{
+		combat_t(data::character_t&);
+	};
+}
+}
+}
