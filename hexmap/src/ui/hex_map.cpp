dba146f422f399fa5e91bb123294e567cafc5b8f
Mathias <Mathias@MF-DESKTOP>
2016-07-09 16:06:33 -0700

    wip hexagon stuff

--- hexmap/src/ui/hex_map.cpp
+++ hexmap/src/ui/hex_map.cpp
@@ -1,26 +1,56 @@
+#include "stdafx.h"
 #include "hex_map.h"
 
+#include <array>
+
+#include "asdf_multiplat/data/gl_resources.h"
+
+namespace asdf
+{
 namespace hexmap
 {
 namespace ui
 {
-    hex_map_t::hex_map_t()
-    {
-    }
-
-    constexpr float hex_halfheight = 0.86602540378; //sin(pi/3)
+    constexpr float hex_halfheight = 0.86602540378f; //sin(pi/3)
     constexpr std::array<float, 18> hexagon_points =
     {
-           1.0f,  0.0f,  0.0f   // mid right
+           1.0f,   0.0f,            0.0f   // mid right
         ,  0.5f,  -hex_halfheight,  0.0f   // bottom right
         , -0.5f,  -hex_halfheight,  0.0f   // bottom left
-        , -1.0f,  0.0f,  0.0f   // middle left
-        , -0.5f,  hex_halfheight,  0.0f   // top left
-        , -0.5f,  hex_halfheight,  0.0f   // top right
+        , -1.0f,   0.0f,            0.0f   // middle left
+        , -0.5f,   hex_halfheight,  0.0f   // top left
+        ,  0.5f,   hex_halfheight,  0.0f   // top right
     };
+
+    const glm::vec4 grid_color(0.0f, 0.0f, 0.0f, 1.0f);
+
+
+    hex_map_t::hex_map_t()
+    {
+        polygon_t verts(6);
+
+        for(size_t i = 0; i < 6; ++i)
+        {
+            verts[i].position.x = hexagon_points[i*3 + 0] * 100;
+            verts[i].position.y = hexagon_points[i*3 + 1] * 100;
+            verts[i].position.z = hexagon_points[i*3 + 2] * 100;
+
+            verts[i].color = grid_color;
+        }
+
+        hexagon.set_data(verts);
+        hexagon.draw_mode = GL_LINE_LOOP;
+    }
 
     void hex_map_t::render_grid_overlay()
     {
+        //test
+        //hexagon.render();
+
+        GL_State.bind(hexagon.vao);
+        glDrawArraysInstanced(hexagon.draw_mode, 0, 6, 10);
+        GL_State.unbind_vao();
     }
+}
 }
 }
--- hexmap/src/ui/hex_map.h
+++ hexmap/src/ui/hex_map.h
@@ -1,20 +1,31 @@
 #pragma once
 
+#include <glm\glm.hpp>
+
+#include "asdf_multiplat/ui/polygon.h"
+#include "asdf_multiplat/ui/ui_base.h"
+
 #include "data/hex_grid.h"
 
+
+using color_t = glm::vec4;
+
+namespace asdf
+{
 namespace hexmap
 {
 namespace ui
 {
     struct hex_map_t
     {
+        ui_polygon_t hexagon;
+
         data::hex_grid_t* hex_grid = nullptr;
-
-
 
         hex_map_t();
 
         void render_grid_overlay();
     };
+}
 }
 }
