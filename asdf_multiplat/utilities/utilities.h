22eb554eff3f93a6b3b778c58747d6684a12ae36
Mathias <Mathias@MF-DESKTOP>
2016-07-09 14:42:04 -0700

    copypasted a for_each function that iterates over a tuple (code from http://codereview.stackexchange.com/a/67394)

--- asdf_multiplat/utilities/utilities.h
+++ asdf_multiplat/utilities/utilities.h
@@ -7,6 +7,9 @@
 #include <vector>
 #include <csignal>
 #include <algorithm>
+#include <cstddef>
+#include <tuple>
+#include <utility>
 
 //#include <boost/random/mersenne_twister.hpp>
 //#include <boost/random/uniform_int_distribution.hpp>
@@ -50,6 +53,26 @@ namespace asdf {
     auto contains(const C& c, const T& x)
     -> decltype(end(c), true)
     { return contains_impl(c, x, 0); }
+
+
+
+    // weird magical tuple iteration from 
+    // http://codereview.stackexchange.com/a/67394
+    template <typename Tuple, typename F, std::size_t ...Indices>
+    void for_each_impl(Tuple&& tuple, F&& f, std::index_sequence<Indices...>) {
+        using swallow = int[];
+        (void)swallow{1,
+            (f(std::get<Indices>(std::forward<Tuple>(tuple))), void(), int{})...
+        };
+    }
+
+    template <typename Tuple, typename F>
+    void for_each(Tuple&& tuple, F&& f) {
+        constexpr std::size_t N = std::tuple_size<std::remove_reference_t<Tuple>>::value;
+        for_each_impl(std::forward<Tuple>(tuple), std::forward<F>(f),
+                      std::make_index_sequence<N>{});
+    }
+    //---
 
 
     /************************************************************************/
