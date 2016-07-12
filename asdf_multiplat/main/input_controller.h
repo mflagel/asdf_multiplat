9de9bfed40922f0360260232ffd16c01665b9dc0
Mathias <Mathias@MF-DESKTOP>
2016-07-11 22:39:47 -0700

    added input_controller_, which holds a position, speed, and a templated input handler. An input handler is in charge of getting and keeping track of the direction the controller should be moving. Added an input handler for keyboard input with SDL2. arrow keys are currently used to control direction. May change it later to have multiple keys controlling an input, and user-defined controls

---
+++ asdf_multiplat/main/input_controller.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <array>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_keyboard.h>
+#include <glm/glm.hpp>
+
+namespace asdf
+{
+namespace input
+{
+    template <typename T>
+    struct input_controller_
+    {
+        T input_handler;
+
+        glm::vec3 position = glm::vec3(0.0f);
+        float speed = 10.f;
+
+        glm::vec3 direction() const { return input_handler.direction; }
+
+        void update(float dt)
+        {
+            position += direction() * speed * dt;
+        }
+
+        void on_event(SDL_Event* event)
+        {
+            input_handler.on_event(event);
+        }
+    };
+
+
+    enum direction_inputs_e
+    {
+          up
+        , down
+        , left
+        , right
+        , num_direction_inputs
+    };
+
+    constexpr std::array<SDL_Keycode, num_direction_inputs> sdl2_input_map =
+    {
+          SDLK_UP
+        , SDLK_DOWN
+        , SDLK_LEFT
+        , SDLK_RIGHT
+    };
+
+    struct sdl2_keyboard_handler_t
+    {
+        std::array<bool, num_direction_inputs> input_state;
+        glm::vec3 direction;
+
+        void on_input_changed();
+
+        void on_event(SDL_Event* event);
+    };
+
+    using input_handler_sdl2_t = input_controller_<sdl2_keyboard_handler_t>;
+}
+}
