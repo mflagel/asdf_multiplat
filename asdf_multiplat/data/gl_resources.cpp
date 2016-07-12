cc63514a01d81df0a499ca445156b5c4612a24a1
Mathias <Mathias@MF-DESKTOP>
2016-07-11 17:49:43 -0700

    added gl_state_t::bind() for shaders

--- asdf_multiplat/data/gl_resources.cpp
+++ asdf_multiplat/data/gl_resources.cpp
@@ -8,14 +8,24 @@ namespace asdf
 
     void gl_state_t::bind(vao_t const& vao)
     {
+        LOG_IF(current_vao == vao.id, "vao %i already in use", vao);
         glBindVertexArray(vao.id);
         current_vao = vao.id;
     }
 
     void gl_state_t::bind(vbo_t const& vbo)
     {
+        LOG_IF(current_vbo == vbo.id, "vbo %i already in use", vbo);
+
         glBindBuffer(GL_ARRAY_BUFFER, vbo.id);
         current_vbo = vbo.id;
+    }
+
+    void gl_state_t::bind(std::shared_ptr<shader_t> const& shader)
+    {
+        //LOG_IF(current_shader == shader->shader_program_id, "shader \'%s\' already in use", shader->name.c_str());
+        glUseProgram(shader->shader_program_id);
+        current_shader = shader->shader_program_id;
     }
 
     void gl_state_t::unbind_vao()
@@ -42,6 +52,7 @@ namespace asdf
         //todo:
         //check vbo
         //check vao
+        //check shader
 
         return true;
     }
