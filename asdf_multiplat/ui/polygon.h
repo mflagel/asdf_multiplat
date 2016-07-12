edd17102004b0d7a4871ccdf127b7048aa59586b
Mathias <Mathias@MF-DESKTOP>
2016-07-11 17:53:15 -0700

    removed polygon_t in favor of rendered_polygon_, which is more generic. Still need the cpp for a static gl_vertex_spec_ rendered_polygon_ now uses the vertex spec to set attributes

--- asdf_multiplat/ui/polygon.h
+++ asdf_multiplat/ui/polygon.h
@@ -4,38 +4,21 @@
 
 #include <glm/glm.hpp>
 
-#include "ui/ui_base.h"
 #include "data/gl_resources.h"
+#include "utilities/utilities_openGL.h"
 
 namespace asdf
 {
 	struct  polygon_vertex_t
 	{
+        static gl_vertex_spec_<vertex_attrib::position3_t, vertex_attrib::color_t> vertex_spec;
+
 		glm::vec3   position;
-		color_t     color;
+		glm::vec4   color;
 	};
 	
 	using polygon_t = std::vector<polygon_vertex_t>;
 
-	struct ui_polygon_t : ui_base_t
-	{
-		vao_t vao;
-		vbo_t vbo;
-		size_t num_verts{0};
-		GLuint draw_mode = GL_TRIANGLES;
-		//---
-
-	    ui_polygon_t();
-	    ui_polygon_t(polygon_t const& points);
-
-        ui_polygon_t(const ui_polygon_t&) = delete;
-	    ui_polygon_t operator=(const ui_polygon_t&) = delete;
-
-	    void set_data(polygon_t const& points);
-
-	    // void render(glm::vec3 const& _position, glm::mat3 const& _matrix, color_t const& _color);
-	    void render();
-	};
 
     template <typename VertexType>
     using polygon_ = std::vector<VertexType>;
@@ -48,36 +31,31 @@ namespace asdf
         rendered_polygon_(polygon_<VertexType> const& verts)
         {}
 
-        void set_vertex_attribs(std::shared_ptr<shader_t> const& shader)
+        void set_data(polygon_<VertexType> const& verts, std::shared_ptr<shader_t> const& shader)
         {
-            size_t stride = sizeof(VertexType);
-
-            GLint posAttrib = glGetAttribLocation(shader->shader_program_id, "Position");
-            glEnableVertexAttribArray(posAttrib);
-            glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, stride, 0);
-
-            GLint colorAttrib = glGetAttribLocation(shader->shader_program_id, "VertColor");
-            glEnableVertexAttribArray(colorAttrib);
-            glVertexAttribPointer(colorAttrib, 4, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
-        }
-
-        void set_data(polygon_<VertexType> const& verts)
-        {
-            LOG_IF(CheckGLError(), "Error before polygon_::set_data()");
+            LOG_IF(util::CheckGLError(), "Error before rendered_polygon_::set_data()");
             GL_State.bind(vao);
 
-            glBindBuffer(GL_ARRAY_BUFFER, vbo);
-            glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(polygon_vertex_t), (void*)(points.data()), GL_STATIC_DRAW);
+            GL_State.bind(vbo);
+            glBufferData(GL_ARRAY_BUFFER, verts.size() * sizeof(polygon_vertex_t), (void*)(verts.data()), GL_STATIC_DRAW);
 
-            set_vertex_attribs(Content.shaders["colored"]);  //TODO: either store a shader in polygon or take one as an argument
+            //set_vertex_attribs(shader);  //not sure the shader should be taken as an arg
+            VertexType::vertex_spec.set_vertex_attribs(shader);
 
             GL_State.unbind_vao();
             GL_State.unbind_vbo();
 
-            num_verts = points.size();
+            num_verts = verts.size();
 
-            LOG_IF(CheckGLError(), "Error during polygon creation");
-            LOG("setup polygon vbo %i with %zu vertexes", vbo.id, points.size());
+            LOG_IF(util::CheckGLError(), "Error during rendered_polygon_::set_data()");
+            LOG("setup polygon vbo %i with %zu vertexes", vbo.id, verts.size());
+        }
+
+        void render()
+        {
+            GL_State.bind(vao);
+            glDrawArrays(darw_mode, 0, num_verts);
+            GL_State.unbind_vao();
         }
     };
 
