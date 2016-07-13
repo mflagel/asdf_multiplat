d4845a3431d687defd1c4ea7f784e2750b6864ce
Mathias <Mathias@MF-DESKTOP>
2016-07-13 14:04:33 -0700

    ui::hex_map_t now creates an array of tile colors and sends it to the uniform array also creates a VBO of tile IDs, and uses glVertexAttribDivisor to have that attribute update once per instance instead of once per vertex changed hexagon render order to render a group of columns instead of a group of rows hexagon tiles now draw, and have TileIDs, but for some reason they don't render the correct color yet

--- hexmap/src/ui/hex_map.cpp
+++ hexmap/src/ui/hex_map.cpp
@@ -21,7 +21,6 @@ namespace ui
     constexpr float hex_height_d2 = hex_height / 2.0f;
     constexpr float hex_height_d4 = hex_height_d2 / 2.0f;
 
-
     constexpr std::array<float, 18> hexagon_points =
     {
            hex_width_d2,   0.0f,            0.0f   // mid right
@@ -32,7 +31,10 @@ namespace ui
         ,  hex_width_d4,   hex_height_d2,  0.0f   // top right
     };
 
-    const glm::vec4 grid_color(0.0f, 0.0f, 0.0f, 1.0f);
+
+    //const glm::vec4 grid_color(0.0f, 0.0f, 0.0f, 1.0f);
+    const glm::vec4 grid_color(1.0f, 1.0f, 1.0f, 1.0f);
+    constexpr float grid_overlay_thickness = 1.0f;
 
 
     hex_map_t::hex_map_t(data::hex_grid_t const& _hex_grid)
@@ -54,35 +56,115 @@ namespace ui
 
         hexagon.set_data(verts, shader);
         hexagon.draw_mode = GL_LINE_LOOP;
+
+        hex_gl_data.set_data(hex_grid.chunks[0][0]);
+        
+        ASSERT(!CheckGLError(), "");
+        GL_State.bind(hexagons_vao);
+
+            GL_State.bind(hexagon.vbo);
+            polygon_vertex_t::vertex_spec.set_vertex_attribs(shader);
+
+
+            GL_State.bind(hex_gl_data);
+            GLint attrib_loc = glGetAttribLocation(shader->shader_program_id, "TileID");
+            glEnableVertexAttribArray(attrib_loc); //tell the location
+            glVertexAttribPointer(attrib_loc, 1, GL_UNSIGNED_INT, false, sizeof(data::hex_tile_id_t), 0);
+            glVertexAttribDivisor(attrib_loc, 1); //second arg is 1, which means the vertex buffer for hexagon tile ID is incremented every instance, instead of every vertex
+
+        GL_State.unbind_vao();
+
+        ASSERT(!CheckGLError(), "");
+
+        std::array<glm::vec4, num_tile_colors> colors =
+        {
+              COLOR_RED
+            , COLOR_GREEN
+            , COLOR_BLUE
+            , COLOR_CYAN
+            , COLOR_YELLOW
+            , COLOR_MAGENTA
+            , COLOR_TEAL
+            , COLOR_ORANGE
+            , COLOR_LIGHTGREY
+            , COLOR_GREY
+        };
+
+        set_tile_colors(colors);
+        ASSERT(!CheckGLError(), "");
     }
+
+    void hex_map_t::set_tile_colors(std::array<glm::vec4, num_tile_colors> const& colors)
+    {
+        GL_State.bind(shader);
+        glUniform4fv(shader->uniform("TileColors[0]"), num_tile_colors, (GLfloat*)colors.data());
+        ASSERT(!CheckGLError(), "");
+    }
+
 
     void hex_map_t::render()
     {
+        //test
+        ASSERT(hex_grid.chunks.size(), "");
+        ASSERT(hex_grid.chunks[0].size(), "");
+        render_chunk(hex_grid.chunks[0][0]);
+
+        glLineWidth(grid_overlay_thickness);
         render_grid_overlay(hex_grid.size);
+    }
+
+    void hex_map_t::render_chunk(data::hex_grid_chunk_t const& chunk)
+    {
+        GL_State.bind(shader);
+        GL_State.bind(hexagons_vao);
+        render_hexagons(glm::ivec2(data::chunk_width, data::chunk_height), GL_TRIANGLE_FAN);
+        GL_State.unbind_vao();
     }
 
     void hex_map_t::render_grid_overlay(glm::ivec2 grid_size)
     {
         GL_State.bind(shader);
-
         GL_State.bind(hexagon.vao);
+        render_hexagons(grid_size, hexagon.draw_mode);
+        GL_State.unbind_vao();
+    }
 
-        for(size_t y = 0; y < grid_size.y * 2; ++y)  //use size.y * 2, since each loop iteration increments a half-height
+    void hex_map_t::render_hexagons(glm::ivec2 grid_size, GLuint draw_mode)
+    {
+        for(size_t x = 0; x < grid_size.x; ++x)
         {
-            bool even_row = (y%2 == 0);
-            shader->world_matrix[3][0] = !even_row * hex_width_d4 * 3;  //offset horizontally for odd y positions
-            shader->world_matrix[3][1] = -hex_height_d2 * y;  //height offset
+            bool even_col = x%2 == 0;
+            shader->world_matrix[3][0] = hex_width_d4 * 3 * x;
+            shader->world_matrix[3][1] = !even_col * -hex_height_d2;
 
             shader->update_wvp_uniform();
 
-            //if an odd width, draw an extra hex every even row
-            size_t n = std::floor(grid_size.x / 2.0f);
-            n += ((grid_size.x % 2 == 1) * even_row);
+            size_t n = (size_t)(grid_size.y);
 
-            glDrawArraysInstanced(hexagon.draw_mode, 0, 6, n); //start at 0th, draw 6 points per shape, draw (width/2)
+            glDrawArraysInstanced(draw_mode, 0, 6, n); //start at 0th, draw 6 points per shape, draw (width/2)
+        }
+    }
+
+
+
+    void hex_buffer_data_t::set_data(data::hex_grid_chunk_t const& chunk)
+    {
+        std::array<data::hex_tile_id_t, data::chunk_width * data::chunk_height> cell_data;
+
+        for(size_t y = 0; y < data::chunk_height; ++y)
+        {
+            for(size_t x = 0; x < data::chunk_width; ++x)
+            {
+                //cell_data[y*data::chunk_height + x] = chunk.cells[x][y].tile_id;
+                cell_data[y*data::chunk_height + x] = rand() % 10;
+                //cell_data[y*data::chunk_height + x] = 1;// + rand() % 3;
+            }
         }
 
-        GL_State.unbind_vao();
+        GL_State.bind(*this);
+        GL_State.buffer_data(*this, (cell_data.size() * sizeof(data::hex_tile_id_t)), (GLvoid*)(cell_data.data()) );
+
+        ASSERT(!CheckGLError(), "Error setting hexagon buffer data");
     }
 }
 }
--- hexmap/src/ui/hex_map.h
+++ hexmap/src/ui/hex_map.h
@@ -16,19 +16,40 @@ namespace hexmap
 {
 namespace ui
 {
+    constexpr size_t num_tile_colors = 10;
+
+
+    struct hex_buffer_data_t : vbo_t //ubo_t  switching to vbo and using glVertexAttribDivisor
+    {
+        hex_buffer_data_t()
+        {
+            usage = GL_DYNAMIC_DRAW;
+        }
+
+        void set_data(data::hex_grid_chunk_t const&);
+    };
+
+
     struct hex_map_t
     {
-        std::shared_ptr<shader_t> shader;
-
-        rendered_polygon_<polygon_vertex_t> hexagon;
-
         data::hex_grid_t const& hex_grid;
 
+        std::shared_ptr<shader_t> shader;
+        rendered_polygon_<polygon_vertex_t> hexagon;
+
+        vao_t hexagons_vao; //used for instancing the hex tiles
+        hex_buffer_data_t hex_gl_data;
+
+        
         hex_map_t(data::hex_grid_t const& hex_grid);
 
+        void set_tile_colors(std::array<glm::vec4, num_tile_colors> const&);
 
         void render();
+
+        void render_chunk(data::hex_grid_chunk_t const&);
         void render_grid_overlay(glm::ivec2 grid_size);
+        void render_hexagons(glm::ivec2 grid_size, GLuint draw_mode);
     };
 }
 }
