6f2529d16a7164e7c4f90d5d64b2ec41a312bebf
Mathias Flagel <mathias.flagel@gmail.com>
2016-07-14 23:13:30 -0700

    hex_grid_t now handles the case where the grid is not evenly divisible by the max chunk size, and will create smaller chunks (allocates the same memory, but only uses part of it

--- hexmap/src/data/hex_grid.cpp
+++ hexmap/src/data/hex_grid.cpp
@@ -1,13 +1,18 @@
 #include "hex_grid.h"
 
 
+using namespace std;
+using namespace glm;
+
 namespace asdf
 {
 namespace hexmap
 {
 namespace data
 {
-    hex_grid_chunk_t::hex_grid_chunk_t(glm::ivec2 size)
+    hex_grid_chunk_t::hex_grid_chunk_t(glm::ivec2 _size)
+    : size(_size)
+    , allocated_size(_size)
     {
         cells.resize(size.x);
 
@@ -22,14 +27,36 @@ namespace data
     hex_grid_t::hex_grid_t(glm::ivec2 _size)
     : size(_size)
     {
-        size_t num_chunks_x = std::ceil((double)size.x / (double)chunk_width);
-        size_t num_chunks_y = std::ceil((double)size.y / (double)chunk_height);
+        // size_t num_chunks_x = std::ceil((double)size.x / (double)max_chunk_width);
+        // size_t num_chunks_y = std::ceil((double)size.y / (double)max_chunk_height);
 
-        chunks.resize(num_chunks_x);
+        auto dv_x = div(size.x, max_chunk_width);
+        auto dv_y = div(size.y, max_chunk_height);
 
-        for(auto& chunk : chunks)
+        size_t num_chunks_x = dv_x.quot + (dv_x.rem > 0);
+        size_t num_chunks_y = dv_y.quot + (dv_y.rem > 0);
+
+        chunks.resize(num_chunks_x); //create the top row
+
+        //cycle through top row and add columns
+        for(size_t x = 0; x < num_chunks_x; ++x)
         {
-            chunk.resize(num_chunks_y);
+            chunks[x].resize(num_chunks_y); //add chunks for this column
+
+            //for each chunk, set position and handle odd edges
+            for(size_t y = 0; y < num_chunks_y; ++y)
+            {
+                //set position
+                chunks[x][y].position = ivec2(x,y);
+
+                //adjust the size of everything on the furthest column
+                if(dv_x.rem > 0 && x == num_chunks_x - 1)
+                    chunks[x][y].size.x = dv_x.rem;
+            }
+
+            //adjust the size of the furthest cells of the row if necessary
+            if(dv_y.rem > 0)
+                chunks[x][chunks[x].size() - 1].size.y = dv_y.rem;
         }
     }
 }
