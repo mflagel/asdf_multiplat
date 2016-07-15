265949da58bce07b04f26d88048c7b88addea551
Mathias Flagel <mathias.flagel@gmail.com>
2016-07-14 23:12:16 -0700

    added hex_grid_t::for_each_chunk() which takes a takes a function and calls it with each chunk added hex_grid_t::num_chunks() util func to make things slightly easier

--- hexmap/src/data/hex_grid.h
+++ hexmap/src/data/hex_grid.h
@@ -11,8 +11,8 @@ namespace hexmap
 {
 namespace data
 {
-    constexpr size_t chunk_width  = 10;
-    constexpr size_t chunk_height = 10;
+    constexpr size_t max_chunk_width  = 10;
+    constexpr size_t max_chunk_height = 10;
 
 
     struct hex_grid_chunk_t;
@@ -29,9 +29,13 @@ namespace data
 
     struct hex_grid_chunk_t
     {
+        glm::ivec2 position; //in chunk units
+        glm::ivec2 size; // how much of the allocated memory is actually used
+        glm::ivec2 allocated_size; //how much memory is actually allocated
+
         hex_cells_t cells;
 
-        hex_grid_chunk_t(glm::ivec2 size = glm::ivec2(chunk_width, chunk_height));
+        hex_grid_chunk_t(glm::ivec2 size = glm::ivec2(max_chunk_width, max_chunk_height));
     };
 
 
@@ -41,6 +45,21 @@ namespace data
     {
         glm::ivec2 size;
         hex_chunks_t chunks;
+
+        size_t num_chunks() const { return size.x * size.y; }
+
+        template<typename F>
+        void for_each_chunk(F f)
+        {
+            for(size_t x = 0; x < chunks.size(); ++x)
+            {
+                for(size_t y = 0; y < chunks[x].size(); ++y)
+                {
+                    chunks[x][y].position = glm::ivec2(x,y);
+                    f(chunks[x][y]);
+                }
+            }
+        }
 
         hex_grid_t(glm::ivec2 size);
     };
