b9a5f853f9723eba982d89cb6827ec3c536ac875
Mathias Flagel <mathias.flagel@gmail.com>
2016-07-14 23:16:05 -0700

    testing rendering chunks of varying size

--- hexmap/src/ui/hex_map.cpp
+++ hexmap/src/ui/hex_map.cpp
@@ -37,7 +37,7 @@ namespace ui
     constexpr float grid_overlay_thickness = 2.0f;
 
 
-    hex_map_t::hex_map_t(data::hex_grid_t const& _hex_grid)
+    hex_map_t::hex_map_t(data::hex_grid_t& _hex_grid)
     : hex_grid(_hex_grid)
     {
 
@@ -56,8 +56,6 @@ namespace ui
 
         hexagon.set_data(verts, shader);
         hexagon.draw_mode = GL_LINE_LOOP;
-
-        hex_gl_data.set_data(hex_grid.chunks[0][0]);
         
         ASSERT(!CheckGLError(), "");
         GL_State.bind(hexagons_vao);
@@ -107,7 +105,12 @@ namespace ui
         //test
         ASSERT(hex_grid.chunks.size(), "");
         ASSERT(hex_grid.chunks[0].size(), "");
-        render_chunk(hex_grid.chunks[0][0]);
+
+        // hex_grid.for_each_chunk( [this](data::hex_grid_chunk_t& chunk) -> void
+        // {
+        //     render_chunk(chunk);
+        // });
+        render_chunk(hex_grid.chunks[1][0]);
 
         glLineWidth(grid_overlay_thickness);
         render_grid_overlay(hex_grid.size);
@@ -117,7 +120,17 @@ namespace ui
     {
         GL_State.bind(shader);
         GL_State.bind(hexagons_vao);
-        render_hexagons(glm::ivec2(data::chunk_width, data::chunk_height), GL_TRIANGLE_FAN);
+
+        float chunk_width_cells = hex_width_d4 * 3 * data::max_chunk_width;
+        float chunk_height_cells = hex_height * data::max_chunk_height;
+
+        shader->world_matrix[3][0] = chunk.position.x * chunk_width_cells;
+        shader->world_matrix[3][1] = chunk.position.y * chunk_height_cells;
+        shader->update_wvp_uniform();
+
+        hex_gl_data.set_data(chunk); //probably more efficient to have one
+
+        render_hexagons(glm::ivec2(chunk.size.x, chunk.size.y), GL_TRIANGLE_FAN);
         GL_State.unbind_vao();
     }
 
@@ -125,12 +138,20 @@ namespace ui
     {
         GL_State.bind(shader);
         GL_State.bind(hexagon.vao);
+
+        shader->world_matrix[3][0] = 0.0f;
+        shader->world_matrix[3][1] = 0.0f;
+        shader->update_wvp_uniform();
+
         render_hexagons(grid_size, hexagon.draw_mode);
         GL_State.unbind_vao();
     }
 
     void hex_map_t::render_hexagons(glm::ivec2 grid_size, GLuint draw_mode)
     {
+        auto loc = shader->uniform("CHUNK_HEIGHT");
+        glUniform1i(loc, grid_size.y);
+
         size_t n = grid_size.x * grid_size.y;
         glDrawArraysInstanced(draw_mode, 0, 6, n); //start at 0th, draw 6 points per shape, draw (width/2)
     }
@@ -139,19 +160,19 @@ namespace ui
 
     void hex_buffer_data_t::set_data(data::hex_grid_chunk_t const& chunk)
     {
-        std::array<data::hex_tile_id_t, data::chunk_width * data::chunk_height> cell_data;
+        std::array<data::hex_tile_id_t, data::max_chunk_width * data::max_chunk_height> cell_data;
 
-        for(size_t y = 0; y < data::chunk_height; ++y)
+        for(size_t x = 0; x < chunk.size.x; ++x)
         {
-            for(size_t x = 0; x < data::chunk_width; ++x)
+            for(size_t y = 0; y < chunk.size.y; ++y)
             {
-                //cell_data[y*data::chunk_height + x] = chunk.cells[x][y].tile_id;
-                cell_data[y*data::chunk_height + x] = rand() % 10;
-                //cell_data[y*data::chunk_height + x] = 1;// + rand() % 3;
+                //cell_data[y*data::chunk_height + x] = rand() % 10;
+                //cell_data[y*chunk.size.y + x] = rand() % 10;
+                cell_data[y*chunk.size.y + x] = chunk.position.x + abs(chunk.position.y);
             }
         }
 
-        GL_State.bind(*this);
+        //GL_State.bind(*this);
         GL_State.buffer_data(*this, (cell_data.size() * sizeof(data::hex_tile_id_t)), (GLvoid*)(cell_data.data()) );
 
         ASSERT(!CheckGLError(), "Error setting hexagon buffer data");
