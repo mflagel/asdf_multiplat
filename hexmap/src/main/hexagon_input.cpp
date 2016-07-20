3beb7294d2a14723346c1f74489cb1d925c0f3b4
Mathias <Mathias@MF-DESKTOP>
2016-07-19 22:33:12 -0700

    compile fixes

--- hexmap/src/main/hexagon_input.cpp
+++ hexmap/src/main/hexagon_input.cpp
@@ -1,8 +1,10 @@
+#include "stdafx.h"  //somehow if this isnt included, MSVC gives weird errors in hexmap.h
 #include "hexagon_input.h"
 
 #include <SDL2/SDL_mouse.h>
 
-#pragma once
+using namespace std;
+using namespace glm;
 
 namespace asdf
 {
@@ -13,18 +15,16 @@ namespace input
     
     hex_map_input_t::hex_map_input_t(data::hex_grid_t& _hex_grid, camera_t& _camera)
     : hex_grid(_hex_grid)
-    , camera(_camera_
+    , camera(_camera)
     , mouse_input(sdl2_mouse_input_t())
     {
-        for(auto& s : mouse_button_states)
-        {
-            s = false;
-        }
+        
     }
 
     glm::ivec2 hex_map_input_t::hex_coords_from_mouse(glm::vec2 mouse_pos)
     {
-
+        EXPLODE("todo");
+        return ivec2(0,0);
     }
 
 
@@ -33,13 +33,15 @@ namespace input
         mouse_input.on_event(event);
 
         //auto hex_coords = 
+
+        return false;
     }
 
 
 /// ----
     constexpr uint8_t mouse_btn_bit(mouse_button_e btn)
     {
-        return ((uint8_t)(btn) + 1)
+        return ((uint8_t)(btn) + 1);
     }
 
     void sdl2_mouse_input_t::on_event(SDL_Event* event)
@@ -47,15 +49,17 @@ namespace input
         switch(event->type)
         {
             case SDL_MOUSEBUTTONDOWN:
+            {
                 mouse_button_states |= mouse_btn_bit(mouse_right)  * (event->button.button == SDL_BUTTON_LEFT);
                 mouse_button_states |= mouse_btn_bit(mouse_right)  * (event->button.button == SDL_BUTTON_RIGHT);
                 mouse_button_states |= mouse_btn_bit(mouse_middle) * (event->button.button == SDL_BUTTON_MIDDLE);
                 mouse_button_states |= mouse_btn_bit(mouse_4)      * (event->button.button == SDL_BUTTON_X1);
                 mouse_button_states |= mouse_btn_bit(mouse_5)      * (event->button.button == SDL_BUTTON_X2);
 
-                mouse_position.x = event->button->x;
-                mouse_position.y = event->button->y;
+                mouse_position.x = event->button.x;
+                mouse_position.y = event->button.y;
                 break;
+            }
 
             case SDL_MOUSEBUTTONUP:
                 mouse_button_states &= mouse_btn_bit(mouse_right)  * (event->button.button != SDL_BUTTON_LEFT);
@@ -64,20 +68,20 @@ namespace input
                 mouse_button_states &= mouse_btn_bit(mouse_4)      * (event->button.button != SDL_BUTTON_X1);
                 mouse_button_states &= mouse_btn_bit(mouse_5)      * (event->button.button != SDL_BUTTON_X2);
 
-                mouse_position.x = event->button->x;
-                mouse_position.y = event->button->y;
+                mouse_position.x = event->button.x;
+                mouse_position.y = event->button.y;
                 break;
 
             case SDL_MOUSEMOTION:
-                mouse_position.x = event->motion->x;
-                mouse_position.y = event->motion->y;
+                mouse_position.x = event->motion.x;
+                mouse_position.y = event->motion.y;
                 break;
 
             case SDL_MOUSEWHEEL:
                 break;
         }
     }
-
+    
 }
 }
 }
--- hexmap/src/main/hexagon_input.h
+++ hexmap/src/main/hexagon_input.h
@@ -2,7 +2,7 @@
 
 #include <array>
 #include <SDL2/SDL.h>
-#include <glm/glm.h>
+#include <glm/glm.hpp>
 
 #include "asdf_multiplat/utilities/camera.h"
 
@@ -22,17 +22,21 @@ namespace input
         , mouse_middle
         , mouse_4
         , mouse_5
-    }
+    };
 
     struct mouse_input_t
     {
-        mouse_button_e mouse_button_states;
+        uint8_t mouse_button_states = 0;
         glm::ivec2 mouse_position;
 
-        constexpr bool mouse_button_state(mouse_button_e btn) const
+        virtual ~mouse_input_t(){}
+
+        bool mouse_button_state(mouse_button_e btn) const
         {
-            return (mouse_button_state & btn) > 0;
+            return (mouse_button_states & (uint8_t)btn) > 0;
         }
+
+        virtual void on_event(SDL_Event* event) = 0;
     };
 
     struct sdl2_mouse_input_t : mouse_input_t
@@ -50,7 +54,7 @@ namespace input
 
         hex_map_input_t(data::hex_grid_t&, camera_t&);
 
-        glm::ivec2 hex_coords_from_mouse(glm::vec2 mouse_pos)
+        glm::ivec2 hex_coords_from_mouse(glm::vec2 mouse_pos);
 
         bool on_event(SDL_Event* event);
     };
