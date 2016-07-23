5d9b4c85daf16d812060b695354f42fcd15ba470
Mathias <Mathias@MF-DESKTOP>
2016-07-23 10:16:25 -0700

    pressing numbers 1 through 0 will change what tile ID you paint on the world

--- hexmap/src/editor/main/input.cpp
+++ hexmap/src/editor/main/input.cpp
@@ -26,12 +26,38 @@ namespace editor
             if(hex_map->hex_grid.is_in_bounds(hx))
             {
                 auto& cell = hex_map->hex_grid.cell_at(hx);
-                cell.tile_id = 5; //test value
+                cell.tile_id = current_tile_id;
                 return true;
             }
         }
 
+        switch(event->type)
+        {
+            case SDL_KEYDOWN:
+                on_key_down(event->key.keysym.sym);
+                break;
+        }
+
         return false;
+    }
+
+    void input_handler_t::on_key_down(SDL_Keycode key)
+    {
+        switch(key)
+        {
+            case SDLK_1: current_tile_id = 1; break;
+            case SDLK_2: current_tile_id = 2; break;
+            case SDLK_3: current_tile_id = 3; break;
+            case SDLK_4: current_tile_id = 4; break;
+            case SDLK_5: current_tile_id = 5; break;
+            case SDLK_6: current_tile_id = 6; break;
+            case SDLK_7: current_tile_id = 7; break;
+            case SDLK_8: current_tile_id = 8; break;
+            case SDLK_9: current_tile_id = 9; break;
+            case SDLK_0: current_tile_id = 0; break;
+        }
+
+        LOG("%i", key);
     }
 
 }
--- hexmap/src/editor/main/input.h
+++ hexmap/src/editor/main/input.h
@@ -8,13 +8,13 @@ namespace editor
 {
     struct input_handler_t : input::hex_map_input_t
     {
+        int current_tile_id = 0;
+
         using input::hex_map_input_t::hex_map_input_t;
 
-        /*input_handler_t(ui::hex_map_t* _hm, camera_t& _c)
-        : input::hex_map_input_t(_hm, _c)
-        {}*/
-
         bool on_event(SDL_Event* event) override;
+
+        void on_key_down(SDL_Keycode key);
     };
 }
 }
