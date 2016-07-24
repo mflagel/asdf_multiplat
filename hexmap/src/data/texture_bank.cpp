9d78c7ac86ba63714e80d7c65e9bb9b5591b2e6e
Mathias <Mathias@MF-DESKTOP>
2016-07-24 14:20:17 -0700

    added texture_bank_t, which is effectively a texture atlas that holds the textures that can be applied to the hexagons. Stores images of equal size in a grid. Resizes input images on load. still a WIP

---
+++ hexmap/src/data/texture_bank.cpp
@@ -0,0 +1,40 @@
+#include "texture_bank.h"
+
+namespace asdf {
+namespace hexmap
+{
+
+    texture_bank_t::texture_bank_t()
+    : atlas_texture("hex texture atlas", nullptr, hex_atlas_dim, hex_atlas_dim)
+    {
+
+    }
+
+    void texture_bank_t::add_texture(std::string const& filepath)
+    {
+        texture_t new_texture("", filepath);
+
+        int dest_loc_x = (saved_textures.size() % max_saved_textures_1d) * saved_texture_dim;
+        int dest_loc_y = (saved_textures.size() / max_saved_textures_1d) * saved_texture_dim;
+
+        glCopyImageSubData( new_texture.texture_id    // GLuint srcName
+                          , GL_TEXTURE_2D             // srcTarget
+                          , 0                         // mipmap level
+                          , 0                         // srcX
+                          , 0                         // srcY
+                          , 0                         // srcZ
+                          , atlas_texture.texture_id  // GLuint dstName
+                          , GL_TEXTURE_2D             // dstTarget
+                          , 0                         // mipmap
+                          , dest_loc_x                // GLint dstX
+                          , dest_loc_y                // GLint dstY
+                          , 0                         // GLint dstZ
+                          , new_texture.width         // GLsizei srcWidth
+                          , new_texture.height        // GLsizei srcHeight
+                          , 0                         // GLsizei srcDepth  might need to be 1?
+                         );
+
+        saved_textures.push_back(saved_texture_t{filepath});
+    }
+}
+}
---
+++ hexmap/src/data/texture_bank.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+#include "asdf_multiplat/data/texture.h"
+
+namespace asdf {
+namespace hexmap
+{
+
+    constexpr size_t saved_texture_dim = 256;
+    constexpr size_t max_saved_textures_1d = 5;
+    constexpr size_t max_saved_textures = max_saved_textures_1d * max_saved_textures_1d;
+    constexpr size_t hex_atlas_dim = saved_texture_dim * max_saved_textures_1d;
+
+
+
+    struct saved_texture_t
+    {
+        std::string filesystem_location;
+        size_t index;
+    };
+
+
+    /*
+        class to load a bunch of textures from the filesystem, resize them,
+        and store them in an atlas
+
+        may use GL_TEXTURE_2D_ARRAY
+            Core in version 4.5
+            Core since version  3.0
+            EXT extension   EXT_texture_array
+
+            Might not exist in mobile, and will have to manually
+            create an atlas
+
+        use glCopyImageSubData to copy data from one texture to another
+            ex: load new texture, copy+resize into bank texture
+
+    */
+    struct texture_bank_t
+    {
+        texture_t atlas_texture;
+
+        std::vector<saved_texture_t> saved_textures;
+
+        texture_bank_t();
+
+        void add_texture(std::string const& filesystem_location);
+    };
+}
+}
