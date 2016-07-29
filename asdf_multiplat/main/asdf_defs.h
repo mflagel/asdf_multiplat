e6d4a6fb38dab3ce682a11f544f8c93def479661
Mathias Flagel <mathias.flagel@gmail.com>
2016-07-29 00:36:14 -0700

    moved some stuff from stdafx into its own header so it can be included without dragging all of stdafx in

---
+++ asdf_multiplat/main/asdf_defs.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#define PI 3.14159265359f
+#define nullindex 4294967295
+
+#define ENDOF(container) container[container.size() - 1]
+
+
+//LOGGING MACROS     ## before VA_ARGS required by clang/gcc
+#define LOG(msg, ...) do{printf(msg, ##__VA_ARGS__); printf("\n");} while(0);
+#define LOG_IF(condition, msg, ...) do{ if(condition){LOG(msg, ##__VA_ARGS__);} }while(0);
+
+
+
+// Color Stuff
+using color_t = glm::vec4;
+
+#define COLOR_CLEAR     color_t(0.0f, 0.0f, 0.0f, 1.0f)
+#define COLOR_WHITE     color_t(1.0f, 1.0f, 1.0f, 1.0f)
+#define COLOR_LIGHTGREY color_t(0.8f, 0.8f, 0.8f, 1.0f)
+#define COLOR_LIGHTGRAY COLOR_LIGHTGREY
+#define COLOR_GREY      color_t(0.5f, 0.5f, 0.5f, 1.0f)
+#define COLOR_GRAY      COLOR_GREY
+#define COLOR_DARKGREY  color_t(0.2f, 0.2f, 0.2f, 1.0f)
+#define COLOR_DARKGRAY  COLOR_DARKGREY
+#define COLOR_BLACK     color_t(0.0f, 0.0f, 0.0f, 1.0f)
+
+#define COLOR_RED       color_t(1.0f, 0.0f, 0.0f, 1.0f)
+#define COLOR_GREEN     color_t(0.0f, 1.0f, 0.0f, 1.0f)
+#define COLOR_BLUE      color_t(0.0f, 0.0f, 1.0f, 1.0f)
+#define COLOR_CYAN      color_t(0.0f, 1.0f, 1.0f, 1.0f)
+#define COLOR_YELLOW    color_t(1.0f, 1.0f, 0.0f, 1.0f)
+#define COLOR_MAGENTA   color_t(1.0f, 0.0f, 1.0f, 1.0f)
+
+#define COLOR_TEAL            color_t(0.0f, 1.0f, 0.5f, 1.0f)
+#define COLOR_ORANGE          color_t(1.0f, 0.5f, 0.0f, 1.0f)
+#define COLOR_CORNFLOWER_BLUE color_t(0.5f, 0.75f, 0.9f, 1.0f)
+
+
+//Assertion code shamelessly copymodified from: 
+//http://cnicholson.net/2009/02/stupid-c-tricks-adventures-in-assert/
+
+#define ASSERTS_ENABLED
+
+//void asdf_fail(char const* condition, char const* file, int line, char const* msg, ...);
+void asdf_fail(char const* condition, char const* file, int line, ...);
+
+#ifdef ASSERTS_ENABLED  
+    #define ASSERT(cond, ...) \
+    do { \
+        if (!(cond)) { \
+            LOG("ASSERT FAILED: %s, %s, %i", #cond, __FILE__, __LINE__) \
+            LOG(__VA_ARGS__); \
+            __debugbreak(); \
+        } \
+    } while(0)
+
+    //like assert(false, ...)
+    #define EXPLODE(...) \
+        do \
+        { \
+            LOG("EXPLOSION: %s, %i", __FILE__, __LINE__); \
+            LOG(__VA_ARGS__);\
+            __debugbreak(); \
+        } while(0)
+#else  
+    #define ASSERT(condition) do { POW2_UNUSED(condition); } while (0)
+    #define EXPLODE(message, ...) do { POW2_UNUSED(condition); } while (0)
+#endif 
