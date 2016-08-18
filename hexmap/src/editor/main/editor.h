13d82a530c07817d265f8bcab3490f1bacac3865
Mathias <Mathias@MF-DESKTOP>
2016-08-17 22:05:33 -0700

    wip on editor tool state and keyboard handling

--- hexmap/src/editor/main/editor.h
+++ hexmap/src/editor/main/editor.h
@@ -12,6 +12,17 @@ namespace editor
 
     struct editor_t : hexmap_t
     {
+        enum tool_type_e
+        {
+              select = 0
+            , terrain_paint
+            , place_object
+            , place_spline
+            , num_tool_types
+        };
+        
+        tool_type_e current_tool = select;
+
         std::unique_ptr<input_handler_t> input;
 
         editor_t();
@@ -19,6 +30,9 @@ namespace editor
         void init() override;
 
         void on_event(SDL_Event*) override;
+
+
+        void set_tool(tool_type_e const& new_tool);
     };
 
 }
--- hexmap/src/editor/main/input.cpp
+++ hexmap/src/editor/main/input.cpp
@@ -3,6 +3,8 @@
 
 #include "asdf_multiplat/main/mouse.h"
 
+#include "editor.h"
+
 namespace asdf
 {
     using namespace input;
@@ -11,6 +13,13 @@ namespace hexmap
 {
 namespace editor
 {
+
+    input_handler_t::input_handler_t(editor_t& _editor)
+    : input::hex_map_input_t(_editor.hex_map.get(), _editor.hex_map->camera)
+    , editor(_editor)
+    {
+    }
+
     bool input_handler_t::on_event(SDL_Event* event)
     {
         input::hex_map_input_t::on_event(event);
@@ -55,9 +64,42 @@ namespace editor
             case SDLK_8: current_tile_id = 8; break;
             case SDLK_9: current_tile_id = 9; break;
             case SDLK_0: current_tile_id = 0; break;
+
+            case SDLK_KP_1: current_object_id = 1; break;
+            case SDLK_KP_2: current_object_id = 2; break;
+            case SDLK_KP_3: current_object_id = 3; break;
+            case SDLK_KP_4: current_object_id = 4; break;
+            case SDLK_KP_5: current_object_id = 5; break;
+            case SDLK_KP_6: current_object_id = 6; break;
+            case SDLK_KP_7: current_object_id = 7; break;
+            case SDLK_KP_8: current_object_id = 8; break;
+            case SDLK_KP_9: current_object_id = 9; break;
+            case SDLK_KP_0: current_object_id = 0; break;
+
+            case SDLK_KP_PERIOD: current_object_id = -1; break;
+
+
+            case sdl2_input_map[cancel_action]:
+                //TODO
+                break;
+
+            case sdl2_input_map[set_tool_select]:
+                editor.set_tool(editor_t::select);
+                break;
+            case set_tool_terrain_paint:
+                editor.set_tool(editor_t::terrain_paint);
+                break;
+            case set_tool_place_object:
+                editor.set_tool(editor_t::place_object);
+                break;
+            case set_tool_place_spline:
+                editor.set_tool(editor_t::place_spline);
+                break;
+
+
         }
 
-        LOG("%i", key);
+        //LOG("%i", key);
     }
 
 }
