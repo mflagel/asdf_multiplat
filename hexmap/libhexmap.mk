e75c15d725a6a50a8e62a3414f79d62360c630d7
Mathias Flagel <mathias.flagel@gmail.com>
2016-09-28 19:30:25 -0700

    made a seperate makefile to compile hexmap as a lib, which will be used in a Qt application

---
+++ hexmap/libhexmap.mk
@@ -0,0 +1,64 @@
+###############################
+PROJNAME = hexmap
+# BIN_OUT = $(BINPATH)/$(PROJNAME)
+
+# INVOCATION = 1
+
+SELF_DIR = $(dir $(lastword $(MAKEFILE_LIST)))
+
+PROJPATH = .
+
+include $(SELF_DIR)/../build/linux/asdf_makefile.inc
+
+# BINPATH = $(PROJPATH)/$(PROJNAME)/bin/linux/x86
+SO_NAME = libhexmap.so
+BIN_OUT = $(LIBPATH)/$(SO_NAME)
+###############################
+
+############ LIBS #############
+LIBS += sdl2 gl glew ftgl
+
+LINK_FLAGS += -L$(LIBPATH) -rpath $(LIBPATH) -lasdfm -lSOIL
+###############################
+
+########## INCLUDES ###########
+INCLUDEPATH = $(PROJPATH)/include
+
+SYSINCLUDES += $(EXTPATH)           \
+			   $(EXTPATH)/glm		\
+			   $(EXTPATH)/cJSON		\
+
+SYSINCLUDES += $(INCLUDEPATH)			\
+			   $(INCLUDEPATH)/SOIL
+
+ADSF_MULTIPLAT_SRC = $(PROJPATH)/asdf_multiplat
+ASDF_MULTIPLAT_INCLUDES = $(ADSF_MULTIPLAT_SRC)/data 	  \
+			  $(ADSF_MULTIPLAT_SRC)/main 	  \
+			  $(ADSF_MULTIPLAT_SRC)/ui 	  \
+			  $(ADSF_MULTIPLAT_SRC)/utilities
+
+SYSINCLUDES += $(PROJPATH) \
+			   $(ADSF_MULTIPLAT_SRC) \
+		       $(ASDF_MULTIPLAT_INCLUDES)
+
+INCLUDES   += $(SRCPATH)          \
+			  $(SRCPATH)/data 	  \
+			  $(SRCPATH)/main 	  \
+			  $(SRCPATH)/ui 	  \
+			  $(SRCPATH)/utilities
+###############################
+
+########### SOURCES ###########
+SRCPATH		= $(PROJPATH)/$(PROJNAME)/src
+
+SOURCES = $(SRCPATH)/main/stdafx.cpp \
+          $(SRCPATH)/data/hex_grid.cpp \
+          $(SRCPATH)/ui/hex_map.cpp \
+          $(SRCPATH)/ui/hex_tile.cpp \
+		  $(EXTPATH)/cJSON/cJSON.c
+
+SRC_FOLDERS += main data ui ../../ext/cJSON
+###############################
+
+# SELF_DIR = $(dir $(lastword $(MAKEFILE_LIST)))
+include $(PROJPATH)/build/linux/asdf_makefile.mk
