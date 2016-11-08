c297bfbebd06372b89831cd397acb9328949313d
Mathias Flagel <mathias.flagel@gmail.com>
2016-11-07 17:11:55 -0800

    moved some mouse state management stuff back into mouse_input_t. library-specific stuff now create the asdf event and send a bit of updated info (in this case mouse position) rather than setting it directly

--- asdf_multiplat/main/mouse.cpp
+++ asdf_multiplat/main/mouse.cpp
@@ -16,4 +16,43 @@ namespace asdf
     {
         return mouse_position - mouse_down_pos;
     }
+
+    void mouse_input_t::mouse_down(mouse_button_event_t& event, glm::ivec2 mouse_pos)
+    {
+        mouse_button_states |= mouse_button_bit(mouse_left)   * (event.button == mouse_left);
+        mouse_button_states |= mouse_button_bit(mouse_right)  * (event.button == mouse_right);
+        mouse_button_states |= mouse_button_bit(mouse_middle) * (event.button == mouse_middle);
+        mouse_button_states |= mouse_button_bit(mouse_4)      * (event.button == mouse_4);
+        mouse_button_states |= mouse_button_bit(mouse_5)      * (event.button == mouse_5);
+
+        mouse_position = mouse_pos;
+        mouse_down_pos = mouse_pos;
+
+        thing->on_mouse_down(event);
+    }
+
+    void mouse_input_t::mouse_up(mouse_button_event_t& event, glm::ivec2 mouse_pos)
+    {
+        mouse_button_states &= mouse_button_bit(mouse_left)   * (event.button != mouse_left);
+        mouse_button_states &= mouse_button_bit(mouse_right)  * (event.button != mouse_right);
+        mouse_button_states &= mouse_button_bit(mouse_middle) * (event.button != mouse_middle);
+        mouse_button_states &= mouse_button_bit(mouse_4)      * (event.button != mouse_4);
+        mouse_button_states &= mouse_button_bit(mouse_5)      * (event.button != mouse_5);
+
+        mouse_position = mouse_pos;
+
+        thing->on_mouse_up(event);
+    }
+
+    void mouse_input_t::mouse_move(mouse_motion_event_t& event, glm::ivec2 mouse_pos)
+    {
+        mouse_position = mouse_pos;
+
+        thing->on_mouse_move(event);
+    }
+
+    void mouse_input_t::mouse_wheel(mouse_wheel_event_t& event)
+    {
+        thing->on_mouse_wheel(event);
+    }
 }
