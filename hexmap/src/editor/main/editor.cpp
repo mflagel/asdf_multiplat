ea7e25132430b89892faf735aa3c90a8eb8faf38
Mathias Flagel <mathias.flagel@gmail.com>
2017-01-07 00:31:10 -0800

    hexmap_t now inits the data::hex_map_t with no size. editor_t instead calls new_map_action() with a default hardcoded size (whole thing will act differently with a Qt UI

--- hexmap/src/editor/main/editor.cpp
+++ hexmap/src/editor/main/editor.cpp
@@ -40,6 +40,12 @@ namespace editor
     {
         hexmap_t::init();
 
+#ifdef DEBUG
+        new_map_action(uvec2(20,20));
+#else
+        new_map_action(uvec2(1,1));
+#endif
+
         input = make_unique<input_handler_t>(*this);
         app.mouse_state.thing = input.get();
     }
