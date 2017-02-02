f2f40b9a2bea61ed08da3edfc7b043776d0e87dc
Mathias Flagel <mathias.flagel@gmail.com>
2017-02-01 19:08:00 -0800

    documentation and a func def for update_WIP_control_nodes()

--- hexmap/src/editor/main/editor.h
+++ hexmap/src/editor/main/editor.h
@@ -111,6 +111,7 @@ namespace editor
         void spline_click(glm::vec2 position);
         void start_spline(data::line_node_t start);
         void update_WIP_node(glm::vec2 const& position);
+        void update_WIP_control_nodes(glm::vec2 const& position);
         void add_node_to_wip_spline(data::line_node_t node);
         void finish_spline(bool spline_loops = false);
         void cancel_spline();
@@ -127,6 +128,55 @@ namespace editor
         , "place_object"
         , "place_spline"
     };
+
+
+
+
+    /*
+        Spline Creation / Modification Notes  (as of Feb 1st 2017)
+
+        Spline Creation Flow:
+        {
+            editor::input_handler_t::on_mouse_down()
+                case editor_t::place_splines:
+                    editor.spline_click(mw);
+
+            editor_t::spline_click()
+                if( !is_placing_spline() )
+                    start_spline(node);
+                else
+                    if(clicking close to first point)
+                        finish_spline()
+                    else
+                        add_node_to_wip_spline(node);
+
+            editor_t::start_spline()
+                create empty spline
+                add start node to spline
+                add a copy of start to act as the WIP node, which will move under the mouse
+                add a control node
+
+            editor_t::add_node_to_wip_spline(new_node)
+                set contents of wip_node to that of new_node
+                add a new node to the list; it is the new wip_node
+                add control point(s) for the new wip node
+    
+        }
+
+
+        Spline Control Point
+        {
+            on spline start
+                add control node (same position as 0th node)
+
+            while dragging with mouse down
+                move position of WIP_control_node (same position as WIP_node)
+
+            on mouse up
+                WIP control point is no longer WIP
+        }
+
+    */
 
 }
 }
