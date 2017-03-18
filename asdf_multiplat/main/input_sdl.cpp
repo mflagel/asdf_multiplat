c5a6543af89466efb28e68dea775a701b5acca69
Mathias Flagel <mathias.flagel@gmail.com>
2017-03-17 17:18:08 -0700

    minor fix

--- asdf_multiplat/main/input_sdl.cpp
+++ asdf_multiplat/main/input_sdl.cpp
@@ -8,7 +8,7 @@ namespace asdf
 {
     void process_sdl_mouse_events(mouse_input_t& mouse, SDL_Event* event)
     {
-        ASSERT(mouse.receiver, "expected thing (I should rename thing)");
+        ASSERT(mouse.receiver, "expected mouse to have a receiver");
 
         switch(event->type)
         {
