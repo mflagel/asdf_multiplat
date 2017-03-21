61a14cfc65d71413b6e1c68188ba66e282b71fe9
Mathias <Mathias@MF-DESKTOP>
2017-03-21 00:56:48 -0700

    object properties widget can now build itself to display some editable values of a map_object

--- hexmap_qt/hexmap_widgets/object_properties_widget.cpp
+++ hexmap_qt/hexmap_widgets/object_properties_widget.cpp
@@ -9,10 +9,12 @@
 
 #include <hexmap/editor/main/editor.h>
 
-template <editable_component_type_e component_type, typename T>
-QWidget* widget_from_component_type(T initial_value = T())
+
+QLayout* layout_from_component_type(editable_component_type_e composite_type)
 {
-    switch(component_type)
+    auto* layout = new QHBoxLayout();
+
+    switch(composite_type)
     {
         case component_int_8:   [[FALLTHROUGH]]
         case component_int_32:  [[FALLTHROUGH]]
@@ -21,60 +23,40 @@ QWidget* widget_from_component_type(T initial_value = T())
         case component_uint_32: [[FALLTHROUGH]]
         case component_uint_64:
         {
-            auto* sb = new QSpinBox();
-            sb->setMinimum(std::numeric_limits<T>::min());
-            sb->setMaximum(std::numeric_limits<T>::max());
-            sb->setValue(initial_value);
-            return sb;
+            layout->addWidget(new QSpinBox());
+            return layout;
         }
 
         case component_float_32:
         case component_float_64:
         {
-            auto* sb = new QDoubleSpinBox();
-            sb->setMinimum(std::numeric_limits<T>::min());
-            sb->setMaximum(std::numeric_limits<T>::max());
-            sb->setValue(initial_value);
-            return sb;
+            layout->addWidget(new QDoubleSpinBox());
+            return layout;
         }
-         default: EXPLODE("TODO: implement this component type");
-    }
 
-
-    /*
-    if(component_type >= component_vec2 && component_type <= component_vec4)
-    {
-        auto* layout = new QHBoxLayout();
-
-        switch(component_type)
+        case component_vec4: [[FALLTHROUGH]]
         {
-            case component_vec4: [[FALLTHROUGH]]
-            {
-                auto* sb = new QDoubleSpinBox();
-                sb->setValue(initial_value.w);
-                layout->addItem(sb);
-            }
-            case component_vec3: [[FALLTHROUGH]]
-            {
-                auto* sb = new QDoubleSpinBox();
-                sb->setValue(initial_value.z);
-                layout->addItem(sb);
-            }
-            case component_vec2:
-            {
-                auto* sbx = new QDoubleSpinBox();
-                auto* sby = new QDoubleSpinBox();
-                sbx->setValue(initial_value.x);
-                sbx->setValue(initial_value.y);
-                layout->addItem(sbx);
-                layout->addItem(sby);
-
-                break;
-            }
-            default: EXPLODE("");
+            auto* sb = new QDoubleSpinBox();
+            layout->addWidget(sb);
         }
-        return layout;
-        */
+        case component_vec3: [[FALLTHROUGH]]
+        {
+            auto* sb = new QDoubleSpinBox();
+            layout->addWidget(sb);
+        }
+        case component_vec2:
+        {
+            auto* sbx = new QDoubleSpinBox();
+            auto* sby = new QDoubleSpinBox();
+            //todo: set defaults
+            layout->addWidget(sbx);
+            layout->addWidget(sby);
+
+            break;
+        }
+        default: EXPLODE("Invalid component type");
+    }
+    return layout;
 }
 
 
@@ -84,6 +66,11 @@ object_properties_widget_t::object_properties_widget_t(QWidget *parent) :
     ui(new Ui::object_properties_widget_t)
 {
     ui->setupUi(this);
+
+    for(size_t i = 0; i < components.size(); ++i)
+    {
+        components[i] = nullptr;
+    }
 }
 
 object_properties_widget_t::~object_properties_widget_t()
@@ -99,16 +86,21 @@ object_properties_widget_t::~object_properties_widget_t()
   I still have to figure out how to give editable fields blank values when selecting multiple objects that have different
   initial values for the property field (ex: display a blank position when selecing two objects in different places)
 */
-void object_properties_widget_t::update(asdf::hexmap::editor::object_selection_t* new_selection)
+void object_properties_widget_t::set_from_object_selection(asdf::hexmap::editor::object_selection_t& selection, std::vector<asdf::hexmap::data::map_object_t>& objects)
 {
-    if(new_selection)
-    {
-        object_selection = new_selection;
-    }
+    object_selection = &selection;
+    object_list = &objects;
 
+    //delete all component edit fields
     delete ui->object_properties_layout;
     ui->object_properties_layout = new QFormLayout();
     ui->properties_holder->addLayout(ui->object_properties_layout);
+
+    //clear component pointers (they're pointing to dead data now anyways)
+    for(size_t i = 0; i < components.size(); ++i)
+    {
+        components[i] = nullptr;
+    }
 
     bool has_objs = object_selection->object_indices.size() > 0;
     bool has_nodes = false;
@@ -116,6 +108,9 @@ void object_properties_widget_t::update(asdf::hexmap::editor::object_selection_t
     if(has_objs && !has_nodes)
     {
         add_fields_map_object();
+        size_t ind = *object_selection->object_indices.begin();
+        ASSERT(object_list->size() >= ind, "there is no object at index %zu", ind);
+        set_from_map_object(object_list->at(ind));
     }
     else if(has_nodes)
     {
@@ -127,6 +122,17 @@ void object_properties_widget_t::update(asdf::hexmap::editor::object_selection_t
 
 }
 
+
+void object_properties_widget_t::add_component_field(obj_component_e component)
+{
+    auto* properties = ui->object_properties_layout;
+
+    ASSERT(!components[component], "component already added");
+
+    components[component] = layout_from_component_type(obj_component_types[component]);
+    properties->addRow(QString(obj_component_names[component]), components[component]);
+}
+
 void object_properties_widget_t::add_fields_map_object()
 {
     using namespace glm;
@@ -134,12 +140,26 @@ void object_properties_widget_t::add_fields_map_object()
     //asdf::hexmap::data::map_object_t* obj = nullptr;
     //if(object_selection->object_indices != 0)
 
-    auto* layout = ui->object_properties_layout;
+    add_component_field(obj_position);
+    add_component_field(obj_size);
+    add_component_field(obj_color);
+    add_component_field(obj_rotation);
+}
 
-    //layout->addRow( "Position", widget_from_component_type<component_vec2, vec2>() );
-    //layout->addRow( "Size",     widget_from_component_type<component_vec2, vec2>() );
-    //layout->addRow( "Color",    widget_from_component_type<component_vec4, vec4>() );
-    layout->addRow( "Rotation", widget_from_component_type<component_float_32, float>() );
+void object_properties_widget_t::set_from_map_object(asdf::hexmap::data::map_object_t const& obj)
+{
+    set_component_sub_value<QDoubleSpinBox>(obj_position, 0, obj.position.x);
+    set_component_sub_value<QDoubleSpinBox>(obj_position, 1, obj.position.y);
+
+    set_component_sub_value<QDoubleSpinBox>(obj_size, 0, obj.size_d2.x * 2);
+    set_component_sub_value<QDoubleSpinBox>(obj_size, 1, obj.size_d2.y * 2);
+
+    set_component_sub_value<QDoubleSpinBox>(obj_color, 0, obj.color.r);
+    set_component_sub_value<QDoubleSpinBox>(obj_color, 1, obj.color.g);
+    set_component_sub_value<QDoubleSpinBox>(obj_color, 2, obj.color.b);
+    set_component_sub_value<QDoubleSpinBox>(obj_color, 3, obj.color.a);
+
+    set_component_value<QDoubleSpinBox>(obj_rotation, obj.rotation);
 }
 
 void object_properties_widget_t::add_fields_line_node()
--- hexmap_qt/hexmap_widgets/object_properties_widget.h
+++ hexmap_qt/hexmap_widgets/object_properties_widget.h
@@ -1,6 +1,8 @@
 #ifndef OBJECT_PROPERTIES_WIDGET_H
 #define OBJECT_PROPERTIES_WIDGET_H
 
+#include <array>
+
 #include <QWidget>
 
 namespace Ui {
@@ -33,6 +35,31 @@ enum editable_component_type_e
     , component_vec4
 };
 
+enum obj_component_e
+{
+      obj_position
+    , obj_size
+    , obj_color
+    , obj_rotation
+    , num_obj_components
+};
+
+constexpr std::array<const char*, num_obj_components> obj_component_names =
+{
+      "Position"
+    , "Size"
+    , "Color"
+    , "Rotation"
+};
+
+constexpr std::array<editable_component_type_e, num_obj_components> obj_component_types =
+{
+      component_vec2
+    , component_vec2
+    , component_vec4
+    , component_float_32
+};
+
 class object_properties_widget_t : public QWidget
 {
     Q_OBJECT
@@ -41,16 +68,41 @@ public:
     explicit object_properties_widget_t(QWidget *parent = 0);
     ~object_properties_widget_t();
 
-    void update(asdf::hexmap::editor::object_selection_t* new_selection);
+    void set_from_object_selection(asdf::hexmap::editor::object_selection_t&, std::vector<asdf::hexmap::data::map_object_t>&);
 
 private:
     asdf::hexmap::editor::object_selection_t* object_selection = nullptr;
-    //std::vector<asdf::hexmap::data::map_object_t>* object_list;
+    std::vector<asdf::hexmap::data::map_object_t>* object_list;
+
+    std::array<QLayout*, num_obj_components> components;
 
     Ui::object_properties_widget_t *ui;
 
+    void add_component_field(obj_component_e);
+
     void add_fields_map_object();
     void add_fields_line_node();
+
+    template <typename QtType, typename T>
+    void set_component_value(obj_component_e component, T value)
+    {
+        set_component_sub_value<QtType>(component, 0, value);
+    }
+
+    template <typename QtType, typename T>
+    void set_component_sub_value(obj_component_e component, size_t index, T value)
+    {
+        ///FIXME modify asdf_multiplat so that I can use ASSERT here without failing to compile
+        if(!components[component])
+            return;
+        if(components[component]->count() <= index)
+            return;
+        //ASSERT(components[component], "");
+        //ASSERT(components[component]->count() > index, "");
+        qobject_cast<QtType*>(components[component]->itemAt(index)->widget())->setValue(value);
+    }
+
+    void set_from_map_object(asdf::hexmap::data::map_object_t const& obj);
 };
 
 #endif // OBJECT_PROPERTIES_WIDGET_H
