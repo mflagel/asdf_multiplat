0393b776d376ebe4c11054cb3663000cddf85a84
Mathias <Mathias@MF-DESKTOP>
2017-04-12 15:49:52 -0700

    fiddling with how camera_t does zoom. It's now just treated like a single multiplier (ex: zoom of 2 is 200% zoom, etc) added aspect_ratio to camera. This is used for setting a viewport from a size but retaining the correct aspect ratio added zoom_for_size() and camera_t::zoom_to_size()

--- asdf_multiplat/utilities/camera.cpp
+++ asdf_multiplat/utilities/camera.cpp
@@ -24,11 +24,22 @@ namespace asdf
                         near_plane, far_plane);
     }
 
+    void camera_t::zoom_to_size(glm::vec2 const& _size)
+    {
+        position.z = glm::sqrt(zoom_for_size(viewport, _size));
+    }
+
     vec3 camera_t::screen_to_world_coord(vec2 const& screen_coord) const
     {
         vec4 vp;
         vp.xy = viewport.bottom_left;
         vp.zw = viewport.size_d2 * 2.0f;
         return unProject(vec3(screen_coord, 0.0f), view_matrix(), projection_ortho(), vp).xyz;
+    }
+
+    float zoom_for_size(viewport_t const& viewport, glm::vec2 const& size)
+    {
+        auto zooms = (size / 2.0f) / viewport.size_d2;
+        return glm::max(zooms.x, zooms.y);
     }
 }
--- asdf_multiplat/utilities/camera.h
+++ asdf_multiplat/utilities/camera.h
@@ -10,16 +10,20 @@ namespace asdf
         glm::vec2 size_d2;
     };
 
+    float zoom_for_size(viewport_t const& viewport, glm::vec2 const& size);
+
     struct camera_t
     {
-        glm::vec3 position;
+        glm::vec3 position  = glm::vec3{0.0f, 0.0f, 1.0f};
         glm::vec3 direction = glm::vec3{0.0f, 0.0f, -1.0f};
         viewport_t viewport;
+        float aspect_ratio = 0.0f;
 
         float near_plane = -1000;
         float  far_plane =  1000;
 
-        float zoom() const { return position.z * position.z; }
+        float zoom() const { return position.z; }
+        void zoom_to_size(glm::vec2 const&);
 
         glm::mat4 view_matrix() const;
         glm::mat4 projection_ortho() const;
