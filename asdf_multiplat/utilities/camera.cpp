6f8b2fba65cd9ed6ba3033ce50b7539a22f56630
Mathias <Mathias@MF-DESKTOP>
2017-04-15 15:04:55 -0700

    camera zoom is now applied as 2^zoom instead of zoom^2 (thus the zoom value is basically a zoom factor) Fixed scren_to_world_coord() to use the current GL screen projection (I should probably change this to use a viewport equal to the actual screen's size)

--- asdf_multiplat/utilities/camera.cpp
+++ asdf_multiplat/utilities/camera.cpp
@@ -3,6 +3,8 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
+#include "data/gl_state.h"
+
 using namespace glm;
 
 namespace asdf
@@ -14,7 +16,7 @@ namespace asdf
 
     glm::mat4 camera_t::projection_ortho() const
     {
-        return projection_ortho(viewport.size_d2 / zoom());
+        return projection_ortho( viewport.size_d2 / glm::pow(2.0f, zoom()) );
     }
 
     mat4 camera_t::projection_ortho(glm::vec2 viewport_d2) const
@@ -31,9 +33,14 @@ namespace asdf
 
     vec3 camera_t::screen_to_world_coord(vec2 const& screen_coord) const
     {
+        auto glvp = GL_State->current_viewport();
         vec4 vp;
-        vp.xy = viewport.bottom_left;
-        vp.zw = viewport.size_d2 * 2.0f;
+        //vp.xy = viewport.bottom_left;
+        //vp.zw = viewport.size_d2 * 2.0f;*/
+
+        vp.zw = vec2{glvp.size};
+        vp.xy = vec2{glvp.bottom_left} - (vec2{glvp.size} / 2.0f);  //screen coords adjusted to have0 in the center, so adjust the viewport
+
         return unProject(vec3(screen_coord, 0.0f), view_matrix(), projection_ortho(), vp).xyz;
     }
 
@@ -47,6 +54,7 @@ namespace asdf
     {
         vec2 sz_d2 = size / 2.0f;
 
+        //use the larger dimension so that nothing inside of size is cut off
         if(sz_d2.x >= sz_d2.y) //if x is bigger, set y
         {
             sz_d2.y = sz_d2.x / aspect_ratio;
--- asdf_multiplat/utilities/camera.h
+++ asdf_multiplat/utilities/camera.h
@@ -15,7 +15,7 @@ namespace asdf
 
     struct camera_t
     {
-        glm::vec3 position  = glm::vec3{0.0f, 0.0f, 1.0f};
+        glm::vec3 position  = glm::vec3{0.0f, 0.0f, 0.0f};
         glm::vec3 direction = glm::vec3{0.0f, 0.0f, -1.0f};
         viewport_t viewport;
         float aspect_ratio = 0.0f;
