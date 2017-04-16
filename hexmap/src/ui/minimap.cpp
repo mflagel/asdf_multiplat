75977a9be903363541854c609a1404d7f793919e
Mathias <Mathias@MF-DESKTOP>
2017-04-15 17:32:32 -0700

    minimap progress

--- hexmap/src/ui/minimap.cpp
+++ hexmap/src/ui/minimap.cpp
@@ -41,8 +41,8 @@ namespace ui
 
         // use glBlendFuncSeparate to allow the minimap FBO to have a transparent background
         // instead of black
-        glEnable(GL_BLEND);
-        glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        //glEnable(GL_BLEND);
+        //glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
         //snapshot the camera
         auto& camera = rendered_map.camera;
@@ -53,13 +53,8 @@ namespace ui
         auto grid_size_d2 = grid_size / 2.0f;
         auto grid_bbox = map_data.hex_grid.bounding_box_units();
 
-        camera.position = vec3(grid_bbox.lower + grid_size_d2, camera.position.z);
-        camera.viewport = viewport_for_size_aspect(map_data.hex_grid.size_units(), 1.0f);
-        //camera.zoom_to_size(grid_size);
-
-        //TEST
-        //camera.position.y -= 5.0f;
-        //
+        camera.position = vec3(grid_bbox.lower + grid_size_d2, 0.0f);
+        camera.viewport = viewport_for_size_aspect(grid_size, 1.0f);
 
         //render just the terrain (and grid outline for now)
         using flags_e = hex_map_t::render_flags_e;
@@ -77,7 +72,7 @@ namespace ui
 
         GL_State->bind(shader);
 
-        float minimap_scale_px = 500.0f;
+        float minimap_scale_px = 400.0f;
 
         shader->world_matrix = scale(translate(mat4(), vec3(0, 0, 0)), vec3(minimap_scale_px));
         shader->view_matrix = mat4();
