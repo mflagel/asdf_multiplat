ffcb1ffb5a5371836751516584c4466804a9c19d
Mathias <Mathias@MF-DESKTOP>
2017-05-13 00:41:00 -0700

    progress towards WIP terrain brush selector widget

--- hexmap_qt/hexmap_widgets/terrain_brush_selector.h
+++ hexmap_qt/hexmap_widgets/terrain_brush_selector.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 
+#include <hexmap/data/terrain_brush.h>
+
 namespace Ui {
 class terrain_brush_selector_t;
 }
@@ -17,13 +19,29 @@ class terrain_brush_selector_t : public QWidget
         , brush_palette
     };
 
+    enum brush_types_e
+    {
+          circular = 0
+        , rectangular
+        , bitmap
+    };
+
 public:
     explicit terrain_brush_selector_t(QWidget *parent = 0);
     ~terrain_brush_selector_t();
 
+public slots:
+    void brush_type_changed(int tab_index);
+
 private:
     void set_brush_tab(tabs_e);
+    void set_brush(brush_types_e);
 
+    float circular_brush_radius() const;
+    glm::uvec2 rect_brush_size() const;
+
+    asdf::hexmap::data::terrain_brush_t terrain_brush;
+    brush_types_e cur_brush_type = circular;
     Ui::terrain_brush_selector_t *ui;
 };
 
