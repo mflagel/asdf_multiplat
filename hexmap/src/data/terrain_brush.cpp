fcdd3c590f7e2a1a75b413523a20005dd17c776d
Mathias <Mathias@MF-DESKTOP>
2017-05-13 20:21:23 -0700

    terrain brush now stores a hex_grid_chunk_t as the mask instead of a 1D std::vector added terrain_brush_hexagon() which uses the hex_util function to generate a spiral of hexes in the shape of a large hexagon

--- hexmap/src/data/terrain_brush.cpp
+++ hexmap/src/data/terrain_brush.cpp
@@ -2,6 +2,8 @@
 
 #include "asdf_multiplat/main/asdf_defs.h"
 
+#include "data/hex_util.hpp"
+
 using namespace std;
 using namespace glm;
 
@@ -11,41 +13,63 @@ namespace hexmap
 {
 namespace data
 {
-    terrain_brush_t::terrain_brush_t(uvec2 size, bool init_val)
+    terrain_brush_t::terrain_brush_t()
+    : brush_mask(glm::uvec2{1,1}, hex_grid_cell_t{1})
     {
-        ASSERT(size.x > 0 && size.y > 0, "Invalid terrain brush size");
-
-        brush_mask.resize(size.x * size.y, init_val);
     }
 
-    terrain_brush_t::terrain_brush_t(terrain_brush_mask_t&& mask)
-    : brush_mask(move(mask))
+    terrain_brush_t::terrain_brush_t(uvec2 size, bool init_val)
+    : brush_mask(size, hex_grid_cell_t{init_val ? 1u : 0u})
     {
+        WARN_IF(size.x > 0 && size.y > 0, "terrain brush size is zero");
     }
 
 
     terrain_brush_t terrain_brush_circle(float radius)
     {
-        terrain_brush_mask_t mask;
-        mask.resize(radius * 2 * radius * 2); //square with width/height equal to diameter
+        terrain_brush_t brush(glm::uvec2{radius * 2, radius * 2});
 
         for(size_t y = 0; y < radius * 2; ++y)
         {
             for(size_t x = 0; x < radius * 2; ++x)
             {
                 auto v = glm::vec2(x,y) - glm::vec2(radius,radius);
-                mask[x*(radius*2) + y] = (length(v) <= radius);
+                brush.brush_mask.cell_at_local_coord(x,y).tile_id = length(v) <= radius;
             }
         }
 
-        return terrain_brush_t(move(mask));
+        return brush;
     }
 
-    terrain_brush_t terrain_brush_from_bitmap(std::string const& filepath)
+    terrain_brush_t terrain_brush_hexagon(float radius)
     {
-        EXPLODE("TODO");
-        return terrain_brush_t();
+        terrain_brush_t brush(glm::uvec2{radius * 2, radius * 2});
+
+        auto spiral = cube_spiral(cube_coord_t{0}, radius);
+
+        for(auto const& cube_coord : spiral)
+        {
+            auto hex_coord = cube_to_hex(cube_coord);
+
+            //shift 0,0 to center of brush
+            //ie: make all coord values positive
+            hex_coord.x += radius;
+            hex_coord.y += radius;
+
+            ASSERT(hex_coord.x > 0 && hex_coord.y > 0, "cannot use negative coords when making a brush");
+
+            auto& cell = brush.brush_mask.cell_at_local_coord(hex_coord);
+            cell.tile_id = 1;
+        }
+
+        return brush;
     }
+
+    //terrain_brush_t terrain_brush_from_bitmap(std::string const& filepath)
+    //{
+    //    EXPLODE("TODO");
+    //    return terrain_brush_t();
+    //}
 
 }
 }
--- hexmap/src/data/terrain_brush.h
+++ hexmap/src/data/terrain_brush.h
@@ -11,20 +11,17 @@ namespace hexmap
 {
 namespace data
 {
-    using terrain_brush_mask_t = std::vector<bool>;
-
     struct terrain_brush_t
     {
-        terrain_brush_mask_t brush_mask;
+        hex_grid_chunk_t brush_mask;
 
-        terrain_brush_t() = default;
-        terrain_brush_t(glm::uvec2 size, bool start_val = true);
-        terrain_brush_t(terrain_brush_mask_t&&);
-
-        //inline glm::uvec2 size() const;
+        terrain_brush_t();
+        terrain_brush_t(glm::uvec2 size, bool start_val = false);
     };
 
     terrain_brush_t terrain_brush_circle(float radius);
+    terrain_brush_t terrain_brush_hexagon(float radius);
+    //terrain_brush_t terrain_brush_from_bitmap(std::string const& filepath);
 }
 }
 }
