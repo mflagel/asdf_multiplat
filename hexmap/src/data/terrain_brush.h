2cf7d21a6a9515a79a4588327ca62c2f4f6cd554
Mathias <Mathias@MF-DESKTOP>
2017-05-14 20:13:51 -0700

    bunch more terrain_brush functionality

--- hexmap/src/data/terrain_brush.h
+++ hexmap/src/data/terrain_brush.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <glm/glm.hpp>
 
+#include "asdf_multiplat/ui/polygon.h"
+
 #include "data/hex_grid.h"
 
 namespace asdf
@@ -17,11 +19,31 @@ namespace data
 
         terrain_brush_t();
         terrain_brush_t(glm::uvec2 size, bool start_val = false);
+
+        inline glm::uvec2 size() const { return brush_mask.size; }
+
+        inline bool cell_is_empty(int x, int y) const { return brush_mask.cell_at_local_coord(x,y).tile_id == 0; }
+
+        inline size_t num_hexes() const { return size().x * size().y; }
+        size_t num_empty_hexes() const;
+        inline size_t num_non_empty_hexes() const { num_hexes() - num_empty_hexes(); }
     };
 
     terrain_brush_t terrain_brush_circle(float radius);
     terrain_brush_t terrain_brush_hexagon(float radius);
     //terrain_brush_t terrain_brush_from_bitmap(std::string const& filepath);
+}
+
+namespace ui
+{
+    struct terrain_brush_vertex_t
+    {
+        static gl_vertex_spec_<vertex_attrib::position3_t/*, vertex_attrib::color_t*/> vertex_spec;
+
+        glm::vec3 position;
+    };
+
+    polygon_<terrain_brush_vertex_t> verts_for_terrain_brush(data::terrain_brush_t const&);
 }
 }
 }
