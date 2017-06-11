530a74907fa0b4cd24bab62e9cce1b5b275522e6
Mathias Flagel <mathias.flagel@gmail.com>
2017-06-11 11:47:50 -0700

    wip argument loading

--- PlantGenerator/src/main.cpp
+++ PlantGenerator/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 #include "plant_generator.h"
 
@@ -7,6 +8,43 @@ using namespace plantgen;
 // undef main because apparently SDL redefines 'main' as SDL_main
 // which is super obnoxious
 #undef main
+
+
+/*  Possible Args *
+-q  : print nothing
+-p  : print pregen nodes
+*/
+
+
+using arg_t = std::pair<std::string, std::string>;
+using args_t = std::unordered_map<std::string, std::string>
+
+arg_t load_arg(int argc, char* argv[], size_t index)
+{
+    ASSRT(index < argc, "arg index beyond argument count");
+    ASSERT(argv[index][0] == '-', "expecting flag specifier to be a dash");
+    arg_t arg;
+
+    arg.first = std::string(argv[index][1]); //skip the '-' character
+
+    if(index+1 < argc && argv[index+1][0] != "-")
+        arg.second = argv[index+1];
+
+    return arg;
+}
+
+args_t load_flags(int argc, char* argv[])
+{
+    args_t args;
+
+    for(size_t i = 1; i < argc; ++i)
+    {
+        if(argv[i][0] == '-')
+            args.insert(load_arg(argc, argv, i));
+    }
+
+    return args;
+}
 
 int main(int argc, char* argv[])
 {
@@ -17,15 +55,27 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    std::string filepath = std::string(argv[1]);
+    auto args = load_flags(argc, argv);
+
+    std::string filepath = std::string(argv[argc-1]);
+
+    generated_node_t plant;
 
     try{
-        generated_node_t plant = generate_node_from_file(filepath);
-        print_node(plant);
+        plant = generate_node_from_file(filepath);
     }
     catch(std::runtime_error const& e)
     {
         cout << e.what() << "\n";
+    }
+
+    if(flags.contains("p"))
+    {
+        print(
+    }
+    if(!flags.contains("q"))
+    {
+        print(plant);
     }
 
     return 0;
