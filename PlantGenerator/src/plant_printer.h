28c9c894dbd78b54fb613d2a64caee395217880f
Mathias Flagel <mathias.flagel@gmail.com>
2017-07-02 23:41:33 -0700

    overhaul to plant_printer to use the data-driven print_string stuff

--- PlantGenerator/src/plant_printer.h
+++ PlantGenerator/src/plant_printer.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include <unordered_map>
+
 #include "plant_generator.h"
 
 namespace plant_printer
 {
+    using string_var_locations_t = std::map<std::string, int>;
+
+    string_var_locations_t parse_print_string(std::string const& print_string);
+
+    std::string print_property_with_string(plantgen::generated_node_t const& plant_node, std::string const& print_string);
+    std::string print_property_with_string(plantgen::generated_node_t const& node);
+
     std::string print_sub_property(plantgen::generated_node_t const& plant_node);
     std::string print_plant(plantgen::generated_node_t const& plant_node);
 }
