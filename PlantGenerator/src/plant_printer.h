ae20dabe476904204f7a516142d1dcb69a793cb4
Mathias Flagel <mathias.flagel@gmail.com>
2017-07-03 14:07:56 -0700

    modifying plant printer to use to_string(generated_node) when there is no PrintString, as it's much easier to read than some generic attempt to english-ify a node

--- PlantGenerator/src/plant_printer.h
+++ PlantGenerator/src/plant_printer.h
@@ -13,6 +13,6 @@ namespace plant_printer
     std::string print_property_with_string(plantgen::generated_node_t const& plant_node, std::string const& print_string);
     std::string print_property_with_string(plantgen::generated_node_t const& node);
 
-    std::string print_sub_property(plantgen::generated_node_t const& plant_node);
+    std::string print_sub_property(plantgen::generated_node_t const& plant_node, size_t level = 0);
     std::string print_plant(plantgen::generated_node_t const& plant_node);
 }
