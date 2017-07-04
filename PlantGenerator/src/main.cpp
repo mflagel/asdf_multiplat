1de997c76265f166a84ab262671505936294ebca
Mathias Flagel <mathias.flagel@gmail.com>
2017-07-04 13:16:19 -0700

    overhaul of flags

--- PlantGenerator/src/main.cpp
+++ PlantGenerator/src/main.cpp
@@ -1,14 +1,17 @@
+#include <array>
 #include <iostream>
 #include <unordered_map>
 #include <string>
 
 #include "asdf_multiplat/main/asdf_defs.h"
+#include "asdf_multiplat/utilities/utilities.h"
 
 #include "plant_generator.h"
 #include "plant_printer.h"
 
 using namespace plantgen;
 using namespace plant_printer;
+using namespace asdf::util;
 
 // undef main because apparently SDL redefines 'main' as SDL_main
 // which is super obnoxious
@@ -19,11 +22,33 @@ using namespace plant_printer;
 -q  : print nothing
 -p  : print pregen nodes
 -g  : print generated nodes
+-s  : print simplified (ie a plain-english description using PlantPrinter)
 */
+
+enum flag_e
+{
+    flag_none              = 0
+  , flag_quiet             = 1
+  , flag_print_pregen      = 2
+  , flag_print_generated   = 4
+  , flag_print_simplified  = 8
+  , flag_all               = 0xFFFFFFFF
+};
+
+using str_lit_vec = std::initializer_list<char const*>; //vector of string literals (stored as char const*)
+constexpr std::initializer_list<str_lit_vec> flag_string_sets =
+{
+      str_lit_vec{}
+    , str_lit_vec{"-q", "--quiet"}
+    , str_lit_vec{"-p", "--print-pregen"}
+    , str_lit_vec{"-g", "--print-generated"}
+    , str_lit_vec{"-s", "--print-simplified"}
+};
 
 
 using arg_t = std::pair<std::string, std::string>;
 using args_t = std::unordered_map<std::string, std::string>;
+using flags_t = uint32_t;
 
 arg_t load_arg(int argc, char* argv[], size_t index)
 {
@@ -40,7 +65,7 @@ arg_t load_arg(int argc, char* argv[], size_t index)
     return arg;
 }
 
-args_t load_flags(int argc, char* argv[])
+args_t load_args(int argc, char* argv[])
 {
     args_t args;
 
@@ -53,6 +78,32 @@ args_t load_flags(int argc, char* argv[])
     return args;
 }
 
+flags_t load_flags(args_t const& args)
+{
+    flags_t flags;
+
+    for(auto const& arg : args)
+    {
+        //search flag_strings for arg
+        int flag_set_index = 0;
+        for(auto const& flag_strings: flag_string_sets)
+        {
+            for(auto const& s : flag_strings)
+            {
+                if(str_eq(arg.first.c_str(), s))
+                {
+                    flags |= 1 << (flag_set_index - 1);
+                    break;
+                }
+            }
+            
+            ++flag_set_index;
+        }
+    }
+
+    return flags;
+}
+
 int main(int argc, char* argv[])
 {
     using namespace std;
@@ -62,7 +113,8 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    auto args = load_flags(argc, argv);
+    args_t args = load_args(argc, argv);
+    flags_t flags = load_flags(args);
 
     std::string filepath = std::string(argv[argc-1]);
 
@@ -76,24 +128,32 @@ int main(int argc, char* argv[])
     catch(std::runtime_error const& e)
     {
         cout << e.what() << "\n";
+        return 0;
     }
 
-    //if no quiet flag
-    if(args.find("-q") == args.end())
-    {
-        bool output_pregen  = args.find("-p") != args.end();
-        bool output_postgen = args.find("-g") != args.end();
+    
+    // if no flags, default to print_simplified
+    // set default before flags is captured in lambda below
+    flags |= (flag_print_simplified * (flags == 0));
 
-        if(output_pregen)
+
+    auto has_flag = [flags](uint _flags) -> bool
+    {
+        return (flags & _flags) > 0;
+    };
+
+
+    if(!has_flag(flag_quiet))
+    {
+        if(has_flag(flag_print_pregen))
         {
             print_node(pregen_node);
         }
-        if(output_postgen)
+        if(has_flag(flag_print_generated))
         {
             print_node(generated_node);
         }
-
-        if(!output_pregen && !output_postgen)
+        if(has_flag(flag_print_simplified))
         {
             generated_node.simplify();
             auto plant_string = print_plant(generated_node);
@@ -102,6 +162,7 @@ int main(int argc, char* argv[])
 
         cout << "\n";
     }
+
 
     return 0;
 }
