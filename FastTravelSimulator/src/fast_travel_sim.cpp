1d3118a117fdc81b3c404897bdc2c3a9a09867f7
Mathias Flagel <mathias.flagel@gmail.com>
2017-07-20 13:44:57 -0700

    large amount of reworking. currently everything at least compiles, and the basic structure of simulating travel over a route is there

--- FastTravelSimulator/src/fast_travel_sim.cpp
+++ FastTravelSimulator/src/fast_travel_sim.cpp
@@ -1,25 +1,48 @@
 #include "fast_travel_sim.h"
 
+#include <plantgen/plant_generator.h>
+#include <plantgen/from_json.h>
+
 using namespace std;
+using namespace plantgen;
+
 
 namespace fast_travel_sim
 {
     namespace
     {
-        constexpr std::string hex_encounter_data_filepath = "../data/HexEncounter.json"
+        constexpr char const* hex_encounter_data_filepath = "../data/HexData.json";
     }
 
-
-    void map_data_t::generate_if_empty(hex_t const& hex)
+    // in order to figure out how far someone can go on a route
+    // that crosses differing terrain difficulties, convert a distance
+    // and slow-rate into a larger distance 
+    int32_t difficulty_modified_dist(int32_t dist, int32_t difficulty)
     {
-        hex_database.try_emplace(hex.coord, hex, hex_generation_data);
+        return dist * difficulty;
     }
 
-    void map_data_t::generate_if_empty(std::vector<hex_t> const& hexes)
+    int32_t difficulty_modified_dist(int32_t dist, hex_t const& hex)
+    {
+        return difficulty_modified_dist(dist, terrain_difficulty(hex));
+    }
+
+    int32_t difficulty_modified_dist(route_segment_t const& route_seg, hex_database_t const& hex_db)
+    {
+        return difficulty_modified_dist(route_seg.dist, hex_db.hex_at(route_seg.coord));
+    }
+
+
+    void hex_database_t::generate_if_empty(hex_t const& hex)
+    {
+        hex_data.try_emplace(hex.coord, hex, hex_rollables);
+    }
+
+    void hex_database_t::generate_if_empty(std::vector<hex_t> const& hexes)
     {
         for(auto const& hex : hexes)
         {
-            generate_if_empty(hex.coord);
+            generate_if_empty(hex);
         }
     }
 
@@ -27,136 +50,198 @@ namespace fast_travel_sim
     generated_hex_t::generated_hex_t(hex_t const& _hex, pregen_node_t const& pregen_node)
     : hex_t(_hex)
     {
-        //TODO:
+        auto gen_node = generate_node(pregen_node);
+
+        /// TODO: search for children by name instead of assuming location
+        ASSERT(gen_node.children.size() == 2, "expected hex generation node to have properties");
+        location = gen_node.children[0];
+        plant = gen_node.children[1];
     }
 
 
 
 
-    bool is_boring(generated_hex_t const& h)
-    {
-        return h.location == ""
-            && h.plant == ""
-            && h.creature == "";
-    }
+    // bool is_boring(generated_hex_t const& h)
+    // {
+    //     return h.location == ""
+    //         && h.plant == ""
+    //         && h.creature == "";
+    // }
 
-    bool is_boring(journal_entry_t const& e)
-    {
-        is_boring(e.hex);
-    }
+    // ///TODO: implement once I have a better understanding of
+    // ///      what a journal_entry_t is going to be 
+    // bool is_boring(journal_entry_t const& e)
+    // {
+    //     //is_boring(e.hex);
+    //     return false;
+    // }
 
 
 
-
+    /// FIXME I think I'll have to create some sort of fake coords
+    /// because otherwise it makes no sense to take a single hex
+    /// and distance, since route_segment_t stores a coord
     journey_route_t build_route(int distance_miles, hex_t hex)
     {
         auto d = std::ldiv(distance_miles, miles_per_hex);
-
-        uint32_t num_hexes = d.quot + d.remainder > 0;
+        uint32_t num_hexes = d.quot + d.rem > 0;
 
         journey_route_t route;
-        route.resize(num_hexes, hex);
+        route.resize(num_hexes, route_segment_t{hex.coord, path_none, miles_per_hex});
 
         return route;
     }
 
 
-    uint32_t hours_required(route_segment_t const& segment)
-    {
-        //segment.dist
-    }
+    // uint32_t hours_required(route_segment_t const& segment)
+    // {
+    //     //segment.dist
+    // }
     
 
-    journal_t make_long_journey(journey_route_t const& route, hex_database_t const& encounter_table_data)
+    /*
+    for each segment
+      travel as far as possible in the time remaining for the day
+        split the segment if only part of it is travelled
+      if the travel budget is empty
+        add a journal entry for the day
+    */
+
+    //take route by copy, as it might get modified later if I split a segment
+    journal_t make_long_journey(journey_route_t route, hex_database_t const& hex_db)
     {
-        std::vector<generated_hex_t> generated_route;
-        generated_route.reserve(route.size());
+        journal_t journal;
+        journal_entry_t current_journal_entry;
+        uint32_t days_spent_travelling = 0;
 
-        size_t days_spent_travelling = 0;
-        size_t current_segment = 0;
-        size_t current_hex_index = 0;
+        int32_t daily_dist_remaining = num_hours_travelling * default_travel_rate;
 
-        size_t distance_remaining = 0;
-        for(auto const& segment : route)
-            distance_remaining += segment.dist;
-
-        while(distance_remaining > 0)
+        for(size_t current_seg_index = 0; current_seg_index < route.size(); ++current_seg_index)
         {
-            auto const& cur_hex = generated_route[current_hex_index];
+            auto const& cur_seg = route[current_seg_index];
+            auto const& cur_hex = hex_db.hex_at(cur_seg.coord);
 
-            //figure out how long it would take to travel the current segment
-            travel_rate = travel_rate();            
+            auto modified_dist = difficulty_modified_dist(cur_seg.dist, cur_hex);
+
+            // if there is travel budget left, travel this segment
+            if(daily_dist_remaining >= modified_dist)
+            {
+                current_journal_entry.hexes.push_back(cur_seg);
+                daily_dist_remaining -= modified_dist;
+            }
+            else
+            {
+                // split segment into travellable and not-travellable
+                int32_t untravelled_dist = modified_dist - daily_dist_remaining;
+                int32_t fraction = daily_dist_remaining / modified_dist;
+
+                route_segment_t new_seg{cur_seg.coord, cur_seg.path, untravelled_dist};
+                route.insert(route.begin() + current_seg_index + 1, std::move(new_seg));
+            }
 
 
-            //if there's enough time left, travel the segment
-            //else split the segment
+            // make camp if there's no more travel remaining for the day
+            if(daily_dist_remaining == 0)
+            {
+                //todo: encounters for the day
 
 
-            //travel segment
+                journal.emplace_back(std::move(current_journal_entry));  /// current_journal_entry will be empty after being moved
+                ++days_spent_travelling;
+            }
         }
 
 
-        //journal_t journal;
-
-        
-
+        return journal;
     }
 
 
 
 
-    std::string summarize(generated_hex_t const& hex)
+    std::string summarize(route_segment_t const& seg)
     {
         std::string summary;
 
-        summary += to_string(hex.coord) + ":";
+        summary += to_string(seg.coord) + ":";
 
-        if(is_boring(hex))
-        {
-            summary += " nothing interesting";
-        }
+        // if(is_boring(hex))
+        // {
+        //     summary += " nothing interesting";
+        // }
+
+        return summary;
     }
 
     std::string summarize(journal_entry_t const& entry)
     {
         std::string summary;
 
-        summary += to_string(entry.hexes.size()) + " hexes traveled\n";
+        auto num_hexes = convert_integer<size_t, uint64_t>(entry.hexes.size());
+        summary += std::to_string(num_hexes) + " hexes traveled\n";
 
         for(auto const& hex : entry.hexes)
         {
             summary += summarize(hex) + "\n";
         }
+
+        return summary;
     }
 
-    std::string summarize(long_journey_results_t const& journal)
+    std::string summarize(journal_t const& journal)
     {
         std::string summary;
 
         summary += "Journey Stats:\n";
-        summary += "    The Journey took " + to_string(journal.size()) + " days";
+        summary += "    The Journey took " + std::to_string(journal.size()) + " days";
 
-        size_t day_index = 0;
+        uint32_t day_index = 0;
 
         for(auto const& entry : journal)
         {
-            summary += "Day " + to_string(day_index + 1) + ":"
+            summary += "Day " + std::to_string(day_index + 1) + ":";
                 summary += summarize(entry);
         }
+
+        return summary;
     }
+}
+
+using namespace fast_travel_sim;
+
+const std::array<hex_t, 4> test_hexes =
+{
+    hex_t{hex_coord_t{0,0}, terrain_flat},
+    hex_t{hex_coord_t{1,0}, terrain_flat},
+    hex_t{hex_coord_t{2,0}, terrain_flat},
+    hex_t{hex_coord_t{3,0}, terrain_flat}
+};
+
+const std::array<route_segment_t, 4> test_route =
+{
+    route_segment_t{hex_coord_t{0,0}, path_none, miles_per_hex},
+    route_segment_t{hex_coord_t{1,0}, path_none, miles_per_hex},
+    route_segment_t{hex_coord_t{2,0}, path_none, miles_per_hex},
+    route_segment_t{hex_coord_t{3,0}, path_none, miles_per_hex}
+};
 
 
-    int main(int argc, char*[] argv)
+int main(int argc, char* argv[])
+{
+    hex_database_t hex_db;
+
+    //TODO: take this as an argument?
+    hex_db.hex_rollables = node_from_json(hex_encounter_data_filepath);
+
+    //TODO: load route from file
+    journey_route_t route = build_route(miles_per_hex * 10, hex_t()); //test route
+
+    for(auto const& hex : test_hexes)
     {
-        hex_database_t map_data;
-
-        //TODO: take this as an argument?
-        map_data.hex_generation_data = node_from_json(hex_encounter_data_filepath);
-
-        //TODO: load route from file
-        journey_route_t route = build_route(miles_per_hex * 10, hex_t()); //test route
-
-        journal_t results = make_long_journey(route, map_data);
-        cout << summarize(journal);
+        hex_db.generate_if_empty(hex);
     }
+
+    journal_t results = make_long_journey(route, hex_db);
+    cout << summarize(results);
+
+    return 0;
 }
