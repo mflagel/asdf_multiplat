0688339980cadc1cddc9fb5774ad690b36b7f7bb
Mathias Flagel <mathias.flagel@gmail.com>
2017-09-05 16:41:06 -0700

    added functions to terrain_brush_selector to grow/shrink/adjust the size of the current brush

--- hexmap_qt/hexmap_widgets/terrain_brush_selector.h
+++ hexmap_qt/hexmap_widgets/terrain_brush_selector.h
@@ -25,6 +25,17 @@ public:
     explicit terrain_brush_selector_t(QWidget *parent = 0);
     ~terrain_brush_selector_t();
 
+    int get_brush_size() const;
+    void set_brush_size(int);
+
+    void shrink_brush(int32_t amt);
+    void grow_brush(int32_t amt);
+    void adjust_brush_size(int32_t amt);
+
+    void shrink_brush();
+    void grow_brush();
+    void adjust_brush_size(bool grow);
+
 public slots:
     void brush_type_changed(int tab_index);
 
@@ -39,7 +50,7 @@ private:
     glm::uvec2 rect_brush_size() const;
 
     asdf::hexmap::data::terrain_brush_t terrain_brush;
-    brush_types_e cur_brush_type = circular;
+    brush_types_e cur_brush_type = hexagonal;
     Ui::terrain_brush_selector_t *ui;
 };
 
