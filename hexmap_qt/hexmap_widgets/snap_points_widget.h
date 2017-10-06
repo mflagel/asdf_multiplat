5dfb11d5f5acddb2019c24a947476c5bcff447ce
Mathias Flagel <mathias.flagel@gmail.com>
2017-10-06 14:14:19 -0700

    more implementation of snap point settings widget

--- hexmap_qt/hexmap_widgets/snap_points_widget.h
+++ hexmap_qt/hexmap_widgets/snap_points_widget.h
@@ -2,21 +2,33 @@
 #define SNAP_POINTS_WIDGET_H
 
 #include <QWidget>
+#include <hexmap/data/hex_util.h>
 
 namespace Ui {
-class snap_points_widget_t;
+    class snap_points_widget_t;
 }
+
+namespace asdf{ namespace hexmap{ namespace editor {
+    struct editor_t;
+}}}
 
 class snap_points_widget_t : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit snap_points_widget_t(QWidget *parent = 0);
+    explicit snap_points_widget_t(asdf::hexmap::editor::editor_t&, QWidget *parent = 0);
     ~snap_points_widget_t();
+
+    asdf::hexmap::editor::editor_t& editor;
 
 private:
     Ui::snap_points_widget_t *ui;
+
+    asdf::hexmap::hex_snap_flags_t snap_flags_from_button_states() const;
+
+private slots:
+    void toggle_snap(bool checked);
 };
 
 #endif // SNAP_POINTS_WIDGET_H
